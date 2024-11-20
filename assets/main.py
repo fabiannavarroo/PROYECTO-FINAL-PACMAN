079e21aabbf11efd50b980ea75dada4422905a98
Bintan20 <ethan.risch@icloud.com>
2024-11-20 17:02:44 +0100

    Auto-commit: 20/11/2024 17:02:44.65

---
+++ assets/main.py
@@ -0,0 +1,17 @@
+import pyxel
+
+class App:
+    def __init__(self):
+        pyxel.init(160,120,"Hello World")
+        pyxel.run(self.update, self.draw)
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_1):
+            for i in range (150):
+                for j in range(110):
+                    pyxel.fill(i,j,7)
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.text(15,15,"BUenos dias",15)
+App()
