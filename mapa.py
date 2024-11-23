e142841d5ae771d5533338af9803344da7a4fa8d
fabiannavarroo <fabynf31@gmail.com>
2024-11-23 01:51:08 +0100

    Auto-commit: Sat Nov 23 01:51:08 CET 2024

--- mapa.py
+++ mapa.py
@@ -38,8 +38,8 @@ class Mapa:
             15: (24, 8),  # Barra izquierda 
             16: (24, 8),  # Barra derecha 
             17: (0, 24),  # Celda rosa fantasmas 
-            18: (24, 8),  # Borde 
-            19: (24, 8),  # Borde 
+            18: (0, 32),  # Subida izquierda 
+            19: (8, 32),  # Subida derecha 
             20: (24, 8),  # Borde 
             21: (24, 8),  # Borde 
             22: (24, 8),  # Borde 
