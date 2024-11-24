46caddd3c64c3b0e0d60a51c4c3c8a90c3ae0e49
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 00:13:48 +0100

    Auto-commit: 25/11/2024  0:13:48,26

--- ONLYMACauto_commit_push.sh
+++ ONLYMACauto_commit_push.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 # Ruta a tu repositorio
-cd /Users/fabiannavarrofonte/Documents/Uni/Programación/PROYECTO-FINAL-PACMAN # Cambia esta ruta por la ubicación de tu repositorio
+cd /Users/fabiannavarrofonte/Documents/Uni/Programación/PROYECTO-FINAL-PACMAN
 
-# Añadir todos los cambios
-echo "Añadiendo cambios..."
-git add .
+# Verificar si hay cambios en el repositorio
+if [[ -n $(git status --porcelain) ]]; then
+    echo "Hay cambios, procesando..."
 
-# Realizar commit con fecha y hora actuales
-echo "Realizando commit..."
-git commit -m "Auto-commit: $(date)"
+    # Hacer pull antes de añadir cambios para evitar conflictos
+    git pull origin main --rebase
 
-# Subir cambios al repositorio
-echo "Subiendo cambios al repositorio..."
-git push origin main
+    # Añadir todos los cambios
+    git add .
 
-echo "¡Cambios subidos correctamente!"
+    # Realizar commit con fecha y hora actuales
+    git commit -m "Auto-commit: $(date)"
+
+    # Subir cambios al repositorio
+    git push origin main
+
+    echo "¡Cambios subidos correctamente!"
+else
+    echo "No hay cambios. Nada que confirmar."
+fi
--- mapa.py
+++ mapa.py
@@ -25,8 +25,20 @@ class Mapa:
             [0,0,0,0,0,8,0,13,14,0,18,22,22,22,22,22,22,20,0,13,14,0,7,0,0,0,0,0],  # Fila 19
             [34,6,6,6,6,4,0,10,12,0,19,23,23,27,32,23,23,21,0,10,12,0,3,6,6,6,6,35],  # Fila 20
             [7,0,0,0,0,0,0,0,0,0,0,0,0,13,14,0,0,0,0,0,0,0,0,0,0,0,0,8],  # Fila 21
+<<<<<<< HEAD
+            [7,0,18,22,22,48,0,18,22,22,22,20,0,13,14,0,18,22,22,22,20,0,46,22,22,20,0,8],  # Fila 22
+            [7,0,19,23,27,14,0,19,23,23,23,21,0,10,12,0,19,23,23,23,21,0,13,46,23,21,0,8],  # Fila 23
+            [7,0,0,0,13,14,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,13,14,0,0,0,8],  # Fila 24
+            [41,22,20,0,13,14,0,9,11,0,18,22,22,22,22,22,22,20,0,9,11,0,13,14,0,18,22,40],  # Fila 25
+            [43,23,21,0,29,31,0,13,14,0,19,23,23,27,32,23,23,21,0,13,14,0,29,31,0,19,23,42],  # Fila 26
+            [7,0,0,0,0,0,0,13,14,0,0,0,0,13,14,0,0,0,0,13,14,0,0,0,0,0,0,8],  # Fila 27
+            [7,0,18,22,22,22,22,31,29,22,22,20,0,13,14,0,18,22,22,31,29,22,22,22,22,20,0,8],  # Fila 28
+            [7,0,19,23,23,23,23,23,23,23,23,21,0,10,12,0,19,23,23,23,23,23,23,23,23,21,0,8],  # Fila 29
+            [7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,8],  # Fila 30
+            [3,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,4] # Fila 31
+=======
             [7,0,18,22,22,20,0,18,22,22,22,20,0,13,14,0,18,22,22,22,20,0,18,22,22,20,0,8],  # Fila 22
-            [7,0,19,23,27,14,0,19,23,23,23,21,0,10,12,0,19,23,23,23,21,0,13,32,23,21,0,8],  # Fila 23
+            [7,0,19,23,27,45,0,19,23,23,23,21,0,10,12,0,19,23,23,23,21,0,13,32,23,21,0,8],  # Fila 23
             [],  # Fila 24
             [],  # Fila 25
             [],  # Fila 26
@@ -35,6 +47,7 @@ class Mapa:
             [],  # Fila 29
             [],  # Fila 30
             [3,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,4,],  # Fila 31
+>>>>>>> c67dbc5 (Auto-commit: Sun Nov 24 21:46:32 CET 2024)
         ]
 
     def draw(self):
@@ -66,18 +79,30 @@ class Mapa:
             21: (16, 40),    # Semi circulo derecha abajo horizontal
             22: (8, 32),   # recta simple arriba horizontal
             23: (8, 40),   # recta simple abajo horizontal
-            24: (,24),  # giro abjo con borde izquierda
-            25: (16, 24),   # giro abjo con borde derecha
+            24: (40, 40),  # giro abjo con borde izquierda
+            25: (48, 40),   # giro abjo con borde derecha
             27: (8, 56),  # cambio de sentido hacia abjo izquierda
             28: (8, 64),   # recta abajo simple para cambio de sentido izquierda
             29: (8, 72),  # cambio de sentido hacia izquierda abajo
             31: (24, 72),  # cambio de sentido hacia arriba derecha
             32: (24, 56),  # cambio de sentido hacia derecha derecha
             33: (24, 64),  # recta simple cambio de sentido derecha
-            34: (0, 80),  # giro de borde izquierda superior
-            35: (8,80),  # giro de borde derecha superior
-            36: (0, 88),  # giro de borde izquierda inferior
-            37: (8, 88)  # giro de borde derecha inferior
+            34: (40, 24),  # giro de borde izquierda superior
+            35: (48, 24),  # giro de borde derecha superior
+            36: (40, 32),  # giro de borde izquierda inferior
+            37: (48, 32),  # giro de borde derecha inferior
+            38: (40, 48),  # giro abajo con borde izquierda
+            39: (48, 48),  # giro abjo con borde derecha
+            40: (40, 56),  # giro izquierda con borde izquierda
+            41: (48, 56),  # giro derecha con borde derecha
+            42: (40, 64),  # giro izquierda con borde izquierda
+            43: (48, 64),  # giro derecha con borde derecha
+            44: (0, 88),   #recta media izquierda cerrado
+            45: (16 , 88),  #recta media derecha cerrado
+            46: (24, 80),  # semicirculo arriba izquierda
+            47: (24, 88),  # semicirculo abajo izquierda
+            48: (32, 80),  # semicirculo arriba derecha
+            49: (32, 88)  # semicirculo abjo derecha
         }
 
         # Dibuja cada celda del mapa en la pantalla
