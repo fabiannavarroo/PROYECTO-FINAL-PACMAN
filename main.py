3548475a4502f84579941e2beb5bf585bd7dd9ba
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 02:13:07 +0100

    Auto-commit: Mon Nov 25 02:13:07 CET 2024

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ class App:
 
         # Inicializa los objetos
         self.mapa = Mapa()
-        self.pacman = Pacman(112, 184)  # Coordenadas iniciales de Pacman
+        self.pacman = Pacman(112, 180)  # Coordenadas iniciales de Pacman
         self.fantasmas = [
             FantasmaRojo(50, 50),
             FantasmaRosa(70, 50),
