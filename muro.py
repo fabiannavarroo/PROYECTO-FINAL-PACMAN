8c8804f000215cb38373f25ec108d98e6f0aa6a7
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 20:56:43 +0100

    Auto-commit: 25/11/2024 20:56:43,86

--- muro.py
+++ muro.py
@@ -1,26 +1,27 @@
-
-
-from constantes import *
 import pyxel
+from constantes import MUROS
 
 class Muro:
     def __init__(self):
-        # Matriz que representa el mapa usando constantes
+        # Matriz que representa el mapa usando números
         self.mapa = [
-            [1,0,1,2,3],
-            [0,0,0,1,1]
+            [1, 2, 2, 2, 3],
+            [9, 0, 0, 0, 10],
+            [9, 0, 13, 14, 10],
+            [9, 0, 0, 0, 10],
+            [4, 5, 5, 5, 6],
         ]
 
     def draw(self):
-        """
-        Dibuja los muros en la pantalla adaptando el tamaño de los sprites.
-        """
+
+        #Dibuja los muros en la pantalla adaptando el tamaño de los sprites.
         for fila in range(len(self.mapa)):
             for columna in range(len(self.mapa[0])):
-                muro = self.mapa[fila][columna]
-                if muro:  # Si no es un espacio vacío
-                    sprite_x, sprite_y = muro["Coordenadas"]
-                    sprite_w, sprite_h = muro["Tamaño"]
+                tipo_muro = self.mapa[fila][columna]
+                if tipo_muro != 0:  # Si no es un espacio vacío
+                    sprite = MUROS[tipo_muro]
+                    sprite_x, sprite_y = sprite["Coordenadas"]
+                    sprite_w, sprite_h = sprite["Tamaño"]
                     pyxel.blt(
                         columna * 8, fila * 8,  # Coordenadas donde se dibuja el muro
                         0,  # Banco de imágenes
