54b65079e2446f9348220929b0c90cc8d1144c22
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 01:38:46 +0100

    Auto-commit: Mon Nov 25 01:38:46 CET 2024

--- pacman.py
+++ pacman.py
@@ -9,7 +9,7 @@ class Pacman:
         self.velocidad = 2  # Velocidad de movimiento
 
     def mover(self):
-        # Movimiento usando teclas
+        # Control de movimiento usando teclas
         if pyxel.btn(pyxel.KEY_UP):
             self.y -= self.velocidad
         if pyxel.btn(pyxel.KEY_DOWN):
@@ -20,6 +20,5 @@ class Pacman:
             self.x += self.velocidad
 
     def dibujar(self):
-        # Dibuja a Pacman en la pantalla
-        # Sprite en posición (0, 0) en el archivo recursos.pyxres
-        pyxel.blt(self.x, self.y, 0, 0, 0, 16, 16, 0)  # Sprite de 16x16 px
+        # Dibuja a Pacman desde la página 0 del banco 0
+        pyxel.blt(self.x, self.y, 0, 0, 0, 16, 16, 0)  # Usa el sprite de Pacman
