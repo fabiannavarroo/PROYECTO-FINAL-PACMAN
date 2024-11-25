c2a8657db2ac2c793196f5fd8e62b77e4d789267
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 20:37:56 +0100

    Auto-commit: 25/11/2024 20:37:56,25

--- constantes.py
+++ constantes.py
@@ -1,33 +1,33 @@
 #---------PACMAN---------#
 PACMAN = (0, 0)  # Coordenadas de Pacman
-PACMAN_ARRIBA = (0, 16)  # Coordenadas de Pacman al moverse arriba
+PACMAN_ARRIBA = (48, 0)  # Coordenadas de Pacman al moverse arriba
 PACMAN_ABAJO = (0, 0)  # Coordenadas de Pacman al moverse abajo
-PACMAN_IZQUIERDA = (0, 32)  # Coordenadas de Pacman al moverse a la izquierda
-PACMAN_DERECHA = (0, 48)  # Coordenadas de Pacman al moverse a la derecha
+PACMAN_IZQUIERDA = (32, 0)  # Coordenadas de Pacman al moverse a la izquierda
+PACMAN_DERECHA = (16, 0)  # Coordenadas de Pacman al moverse a la derecha
 
 #---------FANTASMA ROJO---------#
-FANTASMA_ROJO_ARIBA = (0, 64)  # Coordenadas del fantasma rojo al moverse arriba
-FANTASMA_ROJO_ABAJO = (0, 80)  # Coordenadas del fantasma rojo al moverse abajo
-FANTASMA_ROJO_IZQUIERDA = (0, 96)  # Coordenadas del fantasma rojo al moverse a la izquierda
-FANTASMA_ROJO_DERECHA = (0, 112)  # Coordenadas del fantasma rojo al moverse a la derecha
+FANTASMA_ROJO_ARIBA = (48, 64)  # Coordenadas del fantasma rojo al moverse arriba
+FANTASMA_ROJO_ABAJO = (0, 64)  # Coordenadas del fantasma rojo al moverse abajo
+FANTASMA_ROJO_IZQUIERDA = (16, 64)  # Coordenadas del fantasma rojo al moverse a la izquierda
+FANTASMA_ROJO_DERECHA = (32, 64)  # Coordenadas del fantasma rojo al moverse a la derecha
 
 #---------FANTASMA AZUL---------#
-FANTASMA_AZUL_ARIBA = (0, 128)  # Coordenadas del fantasma azul al moverse arriba
-FANTASMA_AZUL_ABAJO = (0, 144)  # Coordenadas del fantasma azul al moverse abajo
-FANTASMA_AZUL_IZQUIERDA = (0, 160)  # Coordenadas del fantasma azul al moverse a la izquierda
-FANTASMA_AZUL_DERECHA = (0, 176)  # Coordenadas del fantasma azul al moverse a la derecha
+FANTASMA_AZUL_ARIBA = (48, 48)  # Coordenadas del fantasma azul al moverse arriba
+FANTASMA_AZUL_ABAJO = (0, 48)  # Coordenadas del fantasma azul al moverse abajo
+FANTASMA_AZUL_IZQUIERDA = (16, 48)  # Coordenadas del fantasma azul al moverse a la izquierda
+FANTASMA_AZUL_DERECHA = (32, 48)  # Coordenadas del fantasma azul al moverse a la derecha
 
 #---------FANTASMA NARANJA---------#
-FANTASMA_NARANJA_ARIBA = (0, 192)  # Coordenadas del fantasma naranja al moverse arriba
-FANTASMA_NARANJA_ABAJO = (0, 208)  # Coordenadas del fantasma naranja al moverse abajo
-FANTASMA_NARANJA_IZQUIERDA = (0, 224)  # Coordenadas del fantasma naranja al moverse a la izquierda
-FANTASMA_NARANJA_DERECHA = (0, 240)  # Coordenadas del fantasma naranja al moverse a la derecha
+FANTASMA_NARANJA_ARIBA = (48, 80)  # Coordenadas del fantasma naranja al moverse arriba
+FANTASMA_NARANJA_ABAJO = (0, 80)  # Coordenadas del fantasma naranja al moverse abajo
+FANTASMA_NARANJA_IZQUIERDA = (16, 80)  # Coordenadas del fantasma naranja al moverse a la izquierda
+FANTASMA_NARANJA_DERECHA = (32, 80)  # Coordenadas del fantasma naranja al moverse a la derecha
 
 #---------FANTASMA ROSA---------#
-FANTASMA_ROSA_ARIBA = (0, 256)  # Coordenadas del fantasma rosa al moverse arriba
-FANTASMA_ROSA_ABAJO = (0, 272)  # Coordenadas del fantasma rosa al moverse abajo
-FANTASMA_ROSA_IZQUIERDA = (0, 288)  # Coordenadas del fantasma rosa al moverse a la izquierda
-FANTASMA_ROSA_DERECHA = (0, 304)  # Coordenadas del fantasma rosa al moverse a la derecha
+FANTASMA_ROSA_ARIBA = (48, 96)  # Coordenadas del fantasma rosa al moverse arriba
+FANTASMA_ROSA_ABAJO = (0, 96)  # Coordenadas del fantasma rosa al moverse abajo
+FANTASMA_ROSA_IZQUIERDA = (16, 96)  # Coordenadas del fantasma rosa al moverse a la izquierda
+FANTASMA_ROSA_DERECHA = (32, 96)  # Coordenadas del fantasma rosa al moverse a la derecha
 
 #---------MUROS---------#
 1 = {"Coordenadas": (0, 0), "Tamaño": (8, 8)}   # Esquina superior izquierda horizontal
