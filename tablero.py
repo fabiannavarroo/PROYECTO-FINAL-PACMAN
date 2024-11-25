48143526362f034e714bf87d392216b2ff8da56f
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 22:16:21 +0100

    Auto-commit: Mon Nov 25 22:16:21 CET 2024

--- tablero.py
+++ tablero.py
@@ -9,7 +9,7 @@ import pyxel
 
 class Tablero:
     def __init__(self):
-        pyxel.init(448, 496, title="Pacman")  # Dimensiones ajustadas al mapa clásico
+        pyxel.init(448, 494, title="Pacman")  # Dimensiones ajustadas al mapa clásico
         pyxel.load("assets/recursos.pyxres")
 
         # Inicializar el mapa (muros)
