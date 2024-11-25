abadae6a1f99835b4275970fffeb3c384428cf03
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 01:52:39 +0100

    Auto-commit: Mon Nov 25 01:52:39 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -1,5 +1,3 @@
-
-
 import pyxel
 
 class Fantasma:
@@ -9,45 +7,37 @@ class Fantasma:
         self.color = color  # Identifica al fantasma (rojo, rosa, azul, naranja)
         self.direccion = 1  # Dirección inicial: 1 (derecha), -1 (izquierda)
         self.velocidad = 1
+        self.escala = 0.5  # Escala del fantasma (50% más pequeño)
 
     def mover(self):
         # Movimiento horizontal básico
         self.x += self.direccion * self.velocidad
 
         # Cambiar de dirección si alcanza los bordes
-        if self.x < 0 or self.x > pyxel.width - 8:  # Ajustado al nuevo tamaño
+        if self.x < 0 or self.x > pyxel.width - (16 * self.escala):
             self.direccion *= -1
 
     def dibujar(self):
-        # Escala del sprite (más pequeño)
-        escala = 0.5  # Escala del sprite (50% del tamaño original)
-        ancho_original = 16  # Ancho original del sprite
-        alto_original = 16  # Alto original del sprite
+        # Coordenadas del sprite según el color
+        sprite_coords = {
+            "rojo": (0, 64),
+            "rosa": (0, 96),
+            "azul": (0, 48),
+            "naranja": (0, 80)
+        }
+        sprite_x, sprite_y = sprite_coords[self.color]
+        sprite_w = 16  # Ancho original del sprite
+        sprite_h = 16  # Alto original del sprite
 
-        ancho_reducido = int(ancho_original * escala)
-        alto_reducido = int(alto_original * escala)
-
-        # Dibuja al fantasma según su color desde la página 0
-        if self.color == "rojo":
-            pyxel.blt(
-                self.x, self.y, 0, 0, 64,
-                ancho_reducido, alto_reducido, 0
-            )
-        elif self.color == "rosa":
-            pyxel.blt(
-                self.x, self.y, 0, 0, 96,
-                ancho_reducido, alto_reducido, 0
-            )
-        elif self.color == "azul":
-            pyxel.blt(
-                self.x, self.y, 0, 0, 48,
-                ancho_reducido, alto_reducido, 0
-            )
-        elif self.color == "naranja":
-            pyxel.blt(
-                self.x, self.y, 0, 0, 80,
-                ancho_reducido, alto_reducido, 0
-            )
+        # Dibuja el fantasma con escala
+        pyxel.blt(
+            int(self.x), int(self.y), 
+            0,  # Banco de imágenes
+            sprite_x, sprite_y, 
+            int(sprite_w * self.escala),  # Ancho escalado
+            int(sprite_h * self.escala),  # Alto escalado
+            colkey=0  # Transparencia
+        )
 
 # Subclases para cada fantasma
 class FantasmaRojo(Fantasma):
