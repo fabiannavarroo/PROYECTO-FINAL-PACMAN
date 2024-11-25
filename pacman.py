dd9f6d1ea9749ef49434c9c39da8a6248bd23312
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 18:42:59 +0100

    Auto-commit: Mon Nov 25 18:42:59 CET 2024

--- pacman.py
+++ pacman.py
@@ -1,44 +1,32 @@
 
 
-
 import pyxel
 
 class Pacman:
-    def __init__(self, x, y):
-        self.x = x  # Posición inicial en X
-        self.y = y  # Posición inicial en Y
-        self.velocidad = 2  # Velocidad de movimiento
+    def __init__(self, x, y, muro):
+        self.x = x
+        self.y = y
+        self.velocidad = 2
+        self.muro = muro  # Referencia a la clase Muro
 
     def mover(self):
+        nueva_x, nueva_y = self.x, self.y
+
         # Control de movimiento usando teclas
         if pyxel.btn(pyxel.KEY_UP):
-            self.y -= self.velocidad
+            nueva_y -= self.velocidad
         if pyxel.btn(pyxel.KEY_DOWN):
-            self.y += self.velocidad
+            nueva_y += self.velocidad
         if pyxel.btn(pyxel.KEY_LEFT):
-            self.x -= self.velocidad
+            nueva_x -= self.velocidad
         if pyxel.btn(pyxel.KEY_RIGHT):
-            self.x += self.velocidad
-        if pyxel.btn(pyxel.KEY_W):
-            self.y -= self.velocidad
-        if pyxel.btn(pyxel.KEY_S):
-            self.y += self.velocidad
-        if pyxel.btn(pyxel.KEY_A):
-            self.x -= self.velocidad
-        if pyxel.btn(pyxel.KEY_D):
-            self.x += self.velocidad
+            nueva_x += self.velocidad
+
+        # Verificar colisión antes de actualizar la posición
+        if not self.muro.colision(nueva_x, self.y):
+            self.x = nueva_x
+        if not self.muro.colision(self.x, nueva_y):
+            self.y = nueva_y
 
     def draw(self):
-        # Dibuja a Pacman sin reducir el tamaño
-        sprite_x = 0  # Coordenada X en el recurso
-        sprite_y = 0  # Coordenada Y en el recurso
-        sprite_w = 16  # Ancho del sprite
-        sprite_h = 16  # Alto del sprite
-
-        pyxel.blt(
-            self.x, self.y, 
-            0,  # Banco de imágenes
-            sprite_x, sprite_y, 
-            sprite_w, sprite_h, 
-            colkey=0  # Transparencia
-        )
+        pyxel.blt(self.x, self.y, 0, 0, 0, 16, 16, colkey=0)
