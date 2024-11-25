ce71a4a872388201eda208ec9239450738d31a3d
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 19:33:38 +0100

    Auto-commit: Mon Nov 25 19:33:38 CET 2024

--- constantes.py
+++ constantes.py
@@ -30,5 +30,30 @@ FANTASMA_ROSA_IZQUIERDA =
 FANTASMA_ROSA_DERECHA =
 
 #---------MUROS---------#
-    
+Esquina superior izquierda
+Borde superior horizontal
+Esquina superior derecha
+Borde lateral izquierdo
+	5.	Segmento central horizontal
+	6.	Borde lateral derecho
+	7.	Borde superior delgado vertical
+	8.	Esquina superior derecha estrecha
+	9.	Borde lateral delgado izquierdo
+	10.	Segmento central vertical estrecho
+	11.	Borde lateral delgado derecho
+	12.	Esquina inferior derecha estrecha
+	13.	Esquina interior superior izquierda
+	14.	Cruce en forma de T hacia arriba
+	15.	Cruce en forma de T hacia la izquierda
+	16.	Cruce en forma de T hacia la derecha
+	17.	Cruce en forma de T hacia abajo
+	18.	Esquina interior inferior derecha
+	19.	Esquina superior izquierda cuadrada
+	20.	Segmento cuadrado superior horizontal
+	21.	Segmento cuadrado lateral izquierdo
+	22.	Centro cuadrado interior
+	23.	Esquina inferior izquierda rectangular
+	24.	Esquina superior derecha rectangular
+	25.	Esquina inferior izquierda rectangular inferior
+	26.	Esquina inferior derecha rectangular inferior
 
