36c7017bd1bf89b0f5ddd409e6e6cadec1091b2e
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 19:50:04 +0100

    Auto-commit: Mon Nov 25 19:50:04 CET 2024

--- muro.py
+++ muro.py
@@ -16,6 +16,7 @@ class Muro:
         
 
     def colision(self, x, y):
+        
         #Comprueba si hay un muro en la posición (x, y).
         
         fila = y // 16
@@ -23,5 +24,19 @@ class Muro:
 
 
     def draw(self):
+
         #Dibuja los muros en la pantalla adaptando el tamaño de los sprites.
-        
+        for fila in range(len(self.mapa)):
+            for columna in range(len(self.mapa[0])):
+                tipo_muro = self.mapa[fila][columna]
+                if tipo_muro != 0:  # Si no es un espacio vacío
+                    sprite = self.sprites[tipo_muro]
+                    sprite_x, sprite_y = sprite["ubicacion"]
+                    sprite_w, sprite_h = sprite["tamaño"]
+                    pyxel.blt(
+                        columna * 16, fila * 16,  # Coordenadas donde se dibuja el muro
+                        1,  # Banco de imágenes
+                        sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
+                        sprite_w, sprite_h,  # Tamaño del sprite
+                        colkey=0  # Transparencia
+                    )
