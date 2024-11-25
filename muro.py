28a136da2c133b46f07807c167b52e7b826bcd2b
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 20:49:19 +0100

    Auto-commit: 25/11/2024 20:49:19,29

--- muro.py
+++ muro.py
@@ -1,41 +1,29 @@
-import pyxel
+from constantes import *
 
 class Muro:
     def __init__(self):
-        # Matriz que representa el mapa
-        # Cada número en la matriz corresponde a un tipo de muro
+        # Matriz que representa el mapa usando constantes
         self.mapa = [
-            [3, 2, 2, 2, 4],
-            [1, 0, 0, 0, 1],
-            [1, 0, 3, 4, 1],
-            [1, 0, 0, 0, 1],
-            [5, 2, 2, 2, 6],
+            [MURO_ESQUINA_SUPERIOR_IZQUIERDA, MURO_BORDE_SUPERIOR, MURO_BORDE_SUPERIOR, MURO_BORDE_SUPERIOR, MURO_ESQUINA_SUPERIOR_DERECHA],
+            [MURO_VERTICAL_IZQUIERDO, None, None, None, MURO_VERTICAL_DERECHO],
+            [MURO_VERTICAL_IZQUIERDO, None, MURO_CRUCE_T_ARRIBA, None, MURO_VERTICAL_DERECHO],
+            [MURO_VERTICAL_IZQUIERDO, None, None, None, MURO_VERTICAL_DERECHO],
+            [MURO_ESQUINA_INFERIOR_IZQUIERDA, MURO_BORDE_INFERIOR, MURO_BORDE_INFERIOR, MURO_BORDE_INFERIOR, MURO_ESQUINA_INFERIOR_DERECHA],
         ]
 
-        # Diccionario para mapear tipos de muros a sprites y tamaños
-        
-
-    def colision(self, x, y):
-        
-        #Comprueba si hay un muro en la posición (x, y).
-        
-        fila = y // 16
-        columna = x // 16
-
-
     def draw(self):
-
-        #Dibuja los muros en la pantalla adaptando el tamaño de los sprites.
+        """
+        Dibuja los muros en la pantalla adaptando el tamaño de los sprites.
+        """
         for fila in range(len(self.mapa)):
             for columna in range(len(self.mapa[0])):
-                tipo_muro = self.mapa[fila][columna]
-                if tipo_muro != 0:  # Si no es un espacio vacío
-                    sprite = self.sprites[tipo_muro]
-                    sprite_x, sprite_y = sprite["ubicacion"]
-                    sprite_w, sprite_h = sprite["tamaño"]
+                muro = self.mapa[fila][columna]
+                if muro:  # Si no es un espacio vacío
+                    sprite_x, sprite_y = muro["Coordenadas"]
+                    sprite_w, sprite_h = muro["Tamaño"]
                     pyxel.blt(
-                        columna * 16, fila * 16,  # Coordenadas donde se dibuja el muro
-                        1,  # Banco de imágenes
+                        columna * 8, fila * 8,  # Coordenadas donde se dibuja el muro
+                        0,  # Banco de imágenes
                         sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
                         sprite_w, sprite_h,  # Tamaño del sprite
                         colkey=0  # Transparencia
