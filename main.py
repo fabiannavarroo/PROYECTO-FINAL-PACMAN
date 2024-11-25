be15a849c0980cce0864374f2472285abe2441a3
fabiannavarroo <fabynf31@gmail.com>
2024-11-25 12:49:39 +0100

    Auto-commit: Mon Nov 25 12:49:39 CET 2024

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import pyxel
 class App:
     def __init__(self):
         # Inicializa Pyxel
-        pyxel.init(224, 248)  # Tamaño de la ventana
+        pyxel.init(496, 448)  # Tamaño de la ventana
         pyxel.title = "Pacman"
         
         # Carga los recursos desde recursos.pyxres
