1e5bd2780a30c17dda3c9c24e9cf133c0d345368
fabiannavarroo <fabynf31@gmail.com>
2024-11-26 13:23:24 +0100

    Auto-commit: Tue Nov 26 13:23:24 CET 2024

--- tablero.py
+++ tablero.py
@@ -16,7 +16,7 @@ class Tablero:
         self.muro = Muro()
 
         # Inicializar a Pac-Man
-        self.pacman = Pacman(220, 248, self.muro)
+        self.pacman = Pacman(215, 248, self.muro)
 
         # Inicializar los fantasmas
         self.fantasmas = [
