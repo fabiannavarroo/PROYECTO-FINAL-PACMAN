c24105ce929d2b81c865f151e69da4f52b406756
fabiannavarroo <fabynf31@gmail.com>
2024-11-26 19:35:59 +0100

    Auto-commit: Tue Nov 26 19:35:59 CET 2024

--- tablero.py
+++ tablero.py
@@ -1,24 +1,16 @@
-
-
-from constantes import *
 from muro import Muro
 from pacman import Pacman
 from fantasmas import FantasmaRojo, FantasmaRosa, FantasmaAzul, FantasmaNaranja
 import pyxel
 
-
 class Tablero:
     def __init__(self):
-        pyxel.init(430, 333, title="Pacman")  # Dimensiones ajustadas al mapa 
+        pyxel.init(430, 333, title="Pacman")
         pyxel.load("assets/recursos.pyxres")
 
-        # Inicializar el mapa (muros)
         self.muro = Muro()
-
-        # Inicializar a Pac-Man 
         self.pacman = Pacman(208, 240, self.muro)
 
-        # Inicializar los fantasmas
         self.fantasmas = [
             FantasmaRojo(160, 145, self.muro),  # Coordenadas iniciales en la trampa
             FantasmaRosa(176, 145, self.muro),
@@ -30,13 +22,12 @@ class Tablero:
 
     def update(self):
         self.pacman.mover()
-        
         for fantasma in self.fantasmas:
             fantasma.mover()
 
     def draw(self):
         pyxel.cls(0)
-        self.muro.draw()  # Dibujar los muros
-        self.pacman.draw()  # Dibujar Pac-Man
+        self.muro.draw()
+        self.pacman.draw()
         for fantasma in self.fantasmas:
-            fantasma.draw()  # Dibujar fantasmas
+            fantasma.draw()
