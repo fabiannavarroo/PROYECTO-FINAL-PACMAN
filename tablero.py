e28f209e092397683c202d7032181acd1b8a8d90
fabiannavarroo <fabynf31@gmail.com>
2024-11-26 14:38:49 +0100

    Auto-commit: Tue Nov 26 14:38:49 CET 2024

--- tablero.py
+++ tablero.py
@@ -1,31 +1,22 @@
 
 
-
 from muro import Muro
 from pacman import Pacman
 from fantasmas import FantasmaRojo, FantasmaRosa, FantasmaAzul, FantasmaNaranja
 import pyxel
 
-
 class Tablero:
     def __init__(self):
-        pyxel.init(430, 333, title="Pacman")  # Dimensiones ajustadas al mapa 
+        pyxel.init(432, 432, title="Pacman")
         pyxel.load("assets/recursos.pyxres")
-
-        # Inicializar el mapa (muros)
         self.muro = Muro()
-
-        # Inicializar a Pac-Man
         self.pacman = Pacman(208, 240, self.muro)
-
-        # Inicializar los fantasmas
         self.fantasmas = [
-            FantasmaRojo(160, 145, self.muro),  # Coordenadas iniciales en la trampa
-            FantasmaRosa(176, 145, self.muro),
-            FantasmaAzul(192, 145, self.muro),
-        FantasmaNaranja(208, 145, self.muro)
+            FantasmaRojo(208, 224, self.muro),
+            FantasmaRosa(224, 224, self.muro),
+            FantasmaAzul(240, 224, self.muro),
+            FantasmaNaranja(256, 224, self.muro)
         ]
-
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -35,10 +26,8 @@ class Tablero:
 
     def draw(self):
         pyxel.cls(0)
-        self.muro.draw()  # Dibujar los muros
-        self.pacman.draw()  # Dibujar Pac-Man
+        self.muro.draw()
+        self.pacman.draw()
         for fantasma in self.fantasmas:
-            fantasma.draw()  # Dibujar fantasmas
+            fantasma.draw()
 
-# Ejecutar la aplicación
-Tablero()
