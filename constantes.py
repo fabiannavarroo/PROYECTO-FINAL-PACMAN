0b31dc8c6093c5bea511d7609e21617463db7db5
Bintan20 <ethan.risch@icloud.com>
2024-11-27 16:57:11 +0100

    Auto-commit: 27/11/2024 16:57:11.07

--- constantes.py
+++ constantes.py
@@ -61,6 +61,6 @@ MUROS = {
 }
 #-----------PORTALES-----------#
 PORTALES={
-    (-4,144):(420,144), # Portal izquierda
-    (420,144):(-4,144)  # Portal derecha
+    (-6,144):(420,144), # Portal izquierda
+    (420,144):(-6,144)  # Portal derecha
 }
