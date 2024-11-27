b38482adc7ed528f280d663fc3b9acdc72b363d2
fabiannavarroo <fabynf31@gmail.com>
2024-11-27 20:45:08 +0100

    Auto-commit: Wed Nov 27 20:45:08 CET 2024

--- muro.py
+++ muro.py
@@ -50,6 +50,7 @@ class Muro:
             (x + sprite_tamaño - 1, y + sprite_tamaño - 1),  # Esquina inferior derecha
         ]
 
+        # Consegu
         for py in range(y, y + sprite_tamaño, sprite_tamaño - 1):
             for px in range(x, x + sprite_tamaño, sprite_tamaño - 1):
                 fila = int(py // self.celda_tamaño)
