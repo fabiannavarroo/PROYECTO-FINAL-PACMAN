7f5c497adc0d479e292165192a819da5bc8d89ee
fabiannavarroo <fabynf31@gmail.com>
2024-11-27 23:22:26 +0100

    Auto-commit: Wed Nov 27 23:22:26 CET 2024

--- tablero.py
+++ tablero.py
@@ -5,7 +5,7 @@ import pyxel
 
 class Tablero:
     def __init__(self):
-        pyxel.init(430, 415, title="Pacman",display_scale=12,fps=144)  # Tamaño de la Pantalla
+        pyxel.init(430, 415, title="Pacman",display_scale=12,fps=60)  # Tamaño de la Pantalla
         pyxel.load("assets/recursos.pyxres")
 
         self.muro = Muro()
