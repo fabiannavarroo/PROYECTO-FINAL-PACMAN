ad9442502a92d4b65b75f4c780fa745b1925dc8c
fabiannavarroo <fabynf31@gmail.com>
2024-11-28 17:33:50 +0100

    Auto-commit: Thu Nov 28 17:33:50 CET 2024

--- constantes.py
+++ constantes.py
@@ -35,6 +35,7 @@ FANTASMA_NARANJA = {
 BASTON = (48, 16)
 OBJETOS = {
     "REGALO": {"Coordenadas": (0,32), "Puntos": 10},
+    "REGALO_BRILLANTE": {"Coordenadas": (32,32), "Puntos": 10},
     "BASTON": {"Coordenadas": (48,16), "Puntos": 5},
     "CEREZA" : {"Coordenadas": (0, 128), "Puntos": 20},
     "FRESA" : {"Coordenadas":(16, 128), "Puntos": 30},
