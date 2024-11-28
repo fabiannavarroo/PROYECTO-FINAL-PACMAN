d6574197194d9959991e46f954545a5d8ebac6da
Bintan20 <ethan.risch@icloud.com>
2024-11-29 00:02:04 +0100

    Auto-commit: 29/11/2024  0:02:04.78

--- puntos.py
+++ puntos.py
@@ -130,7 +130,7 @@ class Puntos:
                             colkey=0  # Transparencia
                         )
                 if self.muro.mapa[y][x]==89:
-                    sprite = None
+                    sprite = TEXTO["E_BLANCO"]
                     sprite_x,sprite_y = sprite["Coordenadas"]
                     sprite_w,sprite_h = sprite["Tamaño"]
                     pyxel.blt( # Dibujar los imagenes de los regalos
