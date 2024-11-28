439ad61d078f142b8512efffb1fed7bfc533d36f
fabiannavarroo <fabynf31@gmail.com>
2024-11-28 12:33:36 +0100

    Auto-commit: Thu Nov 28 12:33:36 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -1,19 +1,6 @@
 import pyxel
-from constantes import FANTASMA_ROJO, FANTASMA_ROSA, FANTASMA_AZUL, FANTASMA_NARANJA,PORTALES
+from constantes import FANTASMA_ROJO, FANTASMA_ROSA, FANTASMA_AZUL, FANTASMA_NARANJA, PORTALES
 import random
-
-
-class Fantasma:
-    def __init__(self, x, y, sprites, muro):
-        self.x = x
-        self.y = y
-        self.sprites = sprites
-        self.muro = muro
-        self.velocidad = 1
-        self.direccion_actual = "DERECHA"  
-        self.en_trampa = True
-
-    import random
 
 class Fantasma:
     def __init__(self, x, y, sprites, muro):
@@ -34,38 +21,41 @@ class Fantasma:
                 self.en_trampa = False
         else:
             # Direcciones básicas: derecha, izquierda, arriba, abajo
-            direcciones = {
-                "DERECHA": (self.velocidad, 0),
-                "IZQUIERDA": (-self.velocidad, 0),
-                "ARRIBA": (0, -self.velocidad),
-                "ABAJO": (0, self.velocidad),
-            }
+            if self.direccion_actual == "DERECHA":
+                nueva_x = self.x + self.velocidad
+                nueva_y = self.y
+                if not self.muro.colision(nueva_x, nueva_y):
+                    self.x = nueva_x
+                else:
+                    self.direccion_actual = random.choice(["ARRIBA", "ABAJO", "IZQUIERDA"])
+            elif self.direccion_actual == "IZQUIERDA":
+                nueva_x = self.x - self.velocidad
+                nueva_y = self.y
+                if not self.muro.colision(nueva_x, nueva_y):
+                    self.x = nueva_x
+                else:
+                    self.direccion_actual = random.choice(["ARRIBA", "ABAJO", "DERECHA"])
+            elif self.direccion_actual == "ARRIBA":
+                nueva_x = self.x
+                nueva_y = self.y - self.velocidad
+                if not self.muro.colision(nueva_x, nueva_y):
+                    self.y = nueva_y
+                else:
+                    self.direccion_actual = random.choice(["DERECHA", "ABAJO", "IZQUIERDA"])
+            elif self.direccion_actual == "ABAJO":
+                nueva_x = self.x
+                nueva_y = self.y + self.velocidad
+                if not self.muro.colision(nueva_x, nueva_y):
+                    self.y = nueva_y
+                else:
+                    self.direccion_actual = random.choice(["DERECHA", "ARRIBA", "IZQUIERDA"])
 
-            # Movimiento en la dirección actual
-            dx, dy = direcciones[self.direccion_actual]
-            nueva_x = self.x + dx
-            nueva_y = self.y + dy
+            # Teletransporte si está en un portal
+            if (self.x, self.y) in PORTALES:
+                self.x, self.y = PORTALES[(self.x, self.y)]
 
-            # Si choca con un muro, elige una nueva dirección aleatoria
-            if self.muro.colision(nueva_x, nueva_y):
-                direcciones_validas = []
-                for direccion, (dx, dy) in direcciones.items():
-                    if not self.muro.colision(self.x + dx, self.y + dy):
-                        direcciones_validas.append(direccion)
-                if direcciones_validas:
-                    self.direccion_actual = random.choice(direcciones_validas)
-                    dx, dy = direcciones[self.direccion_actual]
-
-            # Actualiza la posición
-            self.x += dx
-            self.y += dy
-
-            #Portal
-            if (self.x,self.y)in PORTALES:
-                self.x,self.y = PORTALES[(self.x,self.y)]
-                
-    # Dibujar el sprite del fantasma en la dirección correspondiente.
     def draw(self):
+        # Dibujar el sprite del fantasma según su dirección
         sprite_x, sprite_y = self.sprites[self.direccion_actual]
         pyxel.blt(self.x, self.y, 0, sprite_x, sprite_y, 16, 16, colkey=0)
 
