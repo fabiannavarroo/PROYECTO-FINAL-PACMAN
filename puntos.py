fd7ea67afa757d6435e7011d655749900daebc15
Bintan20 <ethan.risch@icloud.com>
2024-11-28 23:31:50 +0100

    Auto-commit: 28/11/2024 23:31:50.03

--- puntos.py
+++ puntos.py
@@ -45,4 +45,11 @@ class Puntos:
                     sprite = NUMEROS["0"]
                     sprite_x,sprite_y = sprite["Coordenadas"]
                     sprite_w,sprite_h = sprite["Tamaño"]
+                    pyxel.blt( # Dibujar los imagenes de los regalos
+                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
+                            0,  # Banco de imágenes
+                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
+                            sprite_w, sprite_h,  # Tamaño del sprite
+                            colkey=0  # Transparencia
+                        )
     
