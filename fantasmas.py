ffa2a5993ba56c04d0e4228b906eaf9e50264287
fabiannavarroo <fabynf31@gmail.com>
2024-11-28 11:51:59 +0100

    Auto-commit: Thu Nov 28 11:51:59 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -15,38 +15,19 @@ class Fantasma:
 
     def mover(self):
         if self.en_trampa:
-            # Movimiento para salir de la trampa
-            if not self.muro.colision(self.x, self.y - self.velocidad):
-                self.y -= self.velocidad
-            if self.y < 192:  # Coordenada para salir de la trampa
-                self.en_trampa = False
+            #
         else:
             # Obtener las posibles direcciones
-            direcciones = {
-                "DERECHA": (self.velocidad, 0),
-                "IZQUIERDA": (-self.velocidad, 0),
-                "ARRIBA": (0, -self.velocidad),
-                "ABAJO": (0, self.velocidad),
-            }
+            
 
             # Intentar moverse en la dirección actual
-            dx, dy = direcciones[self.direccion_actual]
-            nueva_x, nueva_y = self.x + dx, self.y + dy
+            
 
             # Si hay colisión, elegir una nueva dirección aleatoria válida
-            if self.muro.colision(nueva_x, nueva_y):
-                direcciones_validas = [
-                    direccion
-                    for direccion, (dx, dy) in direcciones.items()
-                    if not self.muro.colision(self.x + dx, self.y + dy)
-                ]
-                if direcciones_validas:
-                    self.direccion_actual = random.choice(direcciones_validas)
-                    dx, dy = direcciones[self.direccion_actual]
+            
 
             # Actualizar posición
-            self.x += dx
-            self.y += dy
+            
 
             #Portal
             if (self.x,self.y)in PORTALES:
