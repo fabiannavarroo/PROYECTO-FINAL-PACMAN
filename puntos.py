659b7e9aec898b8d15bf88c88bceaf9910e7ae3f
fabiannavarroo <fabynf31@gmail.com>
2024-11-28 13:07:12 +0100

    Auto-commit: Thu Nov 28 13:07:12 CET 2024

--- puntos.py
+++ puntos.py
@@ -1,9 +1,9 @@
-from constantes import 
+from constantes import MINI_PUNTOS
 from muro import MURO
 
 class Puntos:
     def __init__(self,muro):
         self.muro = muro
-        self.sprite = 
+        self.sprite = MINI_PUNTOS
 
     def 
