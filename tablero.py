a2e243e10fbe8455a668b1f0ef1c4a5344d60786
Bintan20 <ethan.risch@icloud.com>
2024-11-28 21:46:09 +0100

    Auto-commit: 28/11/2024 21:46:09.06

--- tablero.py
+++ tablero.py
@@ -29,9 +29,10 @@ class Tablero:
 
     def draw(self):
         pyxel.cls(0)
+        self.pacman.draw()
         self.puntos.draw()
         self.muro.draw()
-        self.pacman.draw()
+        
         for fantasma in self.fantasmas:
             fantasma.draw()
         
