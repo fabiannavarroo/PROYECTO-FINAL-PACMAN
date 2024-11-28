4625c19a3f035535a42d607f8f0d01add711e75f
fabiannavarroo <fabynf31@gmail.com>
2024-11-28 17:26:13 +0100

    Auto-commit: Thu Nov 28 17:26:13 CET 2024

--- muro.py
+++ muro.py
@@ -58,7 +58,7 @@ class Muro:
             # Comprobar si está dentro de los límites del mapa
             if 0 <= fila < len(self.mapa) and 0 <= columna < len(self.mapa[0]):
                 # Si hay un muro, devuelve True
-                if self.mapa[fila][columna] not in [0,,98,99]:
+                if self.mapa[fila][columna] not in [0,98,99]:
                     return True
         return False  # No hay colisión
     
