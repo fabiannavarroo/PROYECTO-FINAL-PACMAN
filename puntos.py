7af4ccf6b4ac6eefbc26c44d11708de0498dc4c0
fabiannavarroo <fabynf31@gmail.com>
2024-11-29 23:49:26 +0100

    Auto-commit: Fri Nov 29 23:49:26 CET 2024

--- puntos.py
+++ puntos.py
@@ -1,27 +1,26 @@
 from constantes import *
 import random
-from muro import Muro
 import time
+from muro import Muro
 import pyxel
 
 class Puntos:
-    def __init__(self,muro,sprite,pacman):
+    def __init__(self, muro, sprite, pacman):
         self.muro = muro
         self.sprite = sprite
         self.pacman = pacman
         self.puntos = 0
-        self.puntos_alcanzados = 0 # Rango de 500 en 500 de la ultima meta alcanzada de puntos 
-        self.color_actual = NUMEROS_BLANCOS # Color de los numeros
+        self.puntos_alcanzados = 0  # Rango de 500 en 500 de la última meta alcanzada de puntos
+        self.color_actual = NUMEROS_BLANCOS  # Color inicial de los números
 
     def draw(self):
-
-        # Poner Los puntos en el mapa
-        for y in range (len(self.muro.mapa)):
+        # Poner los puntos en el mapa
+        for y in range(len(self.muro.mapa)):
             for x in range(len(self.muro.mapa[y])):
                 if self.muro.mapa[y][x] == 0:
                     sprite = OBJETOS["BASTON"]
                     sprite_x, sprite_y = sprite["Coordenadas"]
-                    sprite_w, sprite_h = 16, 16 # Ancho y largo del sprite
+                    sprite_w, sprite_h = 16, 16  # Ancho y largo del sprite
                     pyxel.blt(
                         x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
                         0,  # Banco de imágenes
@@ -29,73 +28,34 @@ class Puntos:
                         sprite_w, sprite_h,  # Tamaño del sprite
                         colkey=0  # Transparencia
                     )
-                
-                # Poner los pastillas de Poder, con su animación
+
+                # Poner los regalos con su animación
                 if self.muro.mapa[y][x] == 98:
                     if pyxel.frame_count // REFRESH_REGALOS % 2:
                         sprite = OBJETOS["REGALO_BRILLANTE"]
                         sprite_x, sprite_y = sprite["Coordenadas"]
-                        sprite_w, sprite_h = 16, 16 # Ancho y largo del sprite
-                        
+                        sprite_w, sprite_h = 16, 16  # Ancho y largo del sprite
                     else:
                         sprite = OBJETOS["REGALO"]
                         sprite_x, sprite_y = sprite["Coordenadas"]
-                        sprite_w, sprite_h = 16, 16 # Ancho y largo del sprite
-                    
-                    pyxel.blt( # Dibujar los imagenes de los regalos
-                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
-                            0,  # Banco de imágenes
-                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
-                            sprite_w, sprite_h,  # Tamaño del sprite
-                            colkey=0  # Transparencia
-                        )
-                if self.muro.mapa[y][x]==97:
-                    sprite = TEXTO["HIGHSCORE"]
-                    sprite_x,sprite_y = sprite["Coordenadas"]
-                    sprite_w,sprite_h = sprite["Tamaño"]
-                    pyxel.blt( # Dibujar los imagenes de los regalos
-                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
-                            0,  # Banco de imágenes
-                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
-                            sprite_w, sprite_h,  # Tamaño del sprite
-                            colkey=0  # Transparencia
-                        )
-                    
-                if self.muro.mapa[y][x]==96:
-                    sprite = TEXTO["READY!"]
-                    sprite_x,sprite_y = sprite["Coordenadas"]
-                    sprite_w,sprite_h = sprite["Tamaño"]
-                    pyxel.blt( # Dibujar los imagenes de los regalos
-                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
-                            0,  # Banco de imágenes
-                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
-                            sprite_w, sprite_h,  # Tamaño del sprite
-                            colkey=0  # Transparencia
-                        )
-                
-                if self.muro.mapa[y][x]==87:
-                    sprite = TEXTO["GAME OVER"]
-                    sprite_x,sprite_y = sprite["Coordenadas"]
-                    sprite_w,sprite_h = sprite["Tamaño"]
-                    pyxel.blt( # Dibujar los imagenes de los regalos
-                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
-                            0,  # Banco de imágenes
-                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
-                            sprite_w, sprite_h,  # Tamaño del sprite
-                            colkey=0  # Transparencia
-                        )
+                        sprite_w, sprite_h = 16, 16  # Ancho y largo del sprite
+
+                    pyxel.blt(
+                        x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
+                        0,  # Banco de imágenes
+                        sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
+                        sprite_w, sprite_h,  # Tamaño del sprite
+                        colkey=0  # Transparencia
+                    )
         self.ver_puntuacion(188, 16)  # Mostrar la puntuación
-                    
 
     def comer_puntos(self):
         pacman_x = self.pacman.x // self.muro.celda_tamaño  # Índice X en el mapa
         pacman_y = self.pacman.y // self.muro.celda_tamaño  # Índice Y en el mapa
 
         if self.muro.mapa[pacman_y][pacman_x] in [0, 98]:
-            # Cuando se tratra de un punto normal
             if self.muro.mapa[pacman_y][pacman_x] == 0:
                 tipo_consumible = "BASTON"
-            # Cuando es una pastilla de poder
             elif self.muro.mapa[pacman_y][pacman_x] == 98:
                 tipo_consumible = "REGALO"
 
@@ -106,29 +66,23 @@ class Puntos:
             self.muro.mapa[pacman_y][pacman_x] = -1
 
     def ver_puntuacion(self, x, y):
-        # Determina el color de los numero
-        colores_dispo = [NUMEROS_BLANCOS,NUMEROS_MORADOS,NUMEROS_NARANJAS,NUMEROS_VERDES]
-        
+        colores_dispo = [NUMEROS_BLANCOS, NUMEROS_MORADOS, NUMEROS_NARANJAS, NUMEROS_VERDES]
+
         # Cambia el color solo cuando se supera un nuevo múltiplo de 500
         if self.puntos // 500 > self.puntos_alcanzados:
             self.puntos_alcanzados = self.puntos // 500  # Actualiza los puntos alcanzados
             self.color_actual = random.choice(colores_dispo)  # Elige un nuevo color aleatorio
-        # Convierte la puntuación en una cadena para obtener los numeros
-        puntuacion_str = str(self.puntos)
 
-        # Coordenada inicial para el primer dígito
+        color_numeros = self.color_actual
+        puntuacion_str = str(self.puntos)
         pos_x = x
 
         for num in puntuacion_str:
-            # Convierte el carácter a un entero
             num = int(num)
-
-            # Obtiene el sprite correspondiente al dígito
-            sprite = self.color_actual[str(num)]
+            sprite = color_numeros[str(num)]
             sprite_x, sprite_y = sprite["Coordenadas"]
             sprite_w, sprite_h = sprite["Tamaño"]
 
-            # Dibuja el número
             pyxel.blt(
                 pos_x, y,           # Coordenadas en pantalla
                 0,                  # Banco de imágenes
@@ -136,9 +90,46 @@ class Puntos:
                 sprite_w, sprite_h, # Tamaño del sprite
                 colkey=0            # Color transparente
             )
-            # Incrementa la posición x para el siguiente dígito
             pos_x += sprite_w + 1  # Espacio entre los dígitos
 
-    def aparecer_frutas(self, x, y):
-        time.sleep(30)
-        
+    def encontrar_celdas_vacias(self):
+        celdas_vacias = []
+        for y in range(len(self.muro.mapa)):
+            for x in range(len(self.muro.mapa[y])):
+                if self.muro.mapa[y][x] == -1:
+                    celdas_vacias.append((x, y))  # Guarda las coordenadas de las celdas vacías
+        return celdas_vacias
+
+    def aparecer_fruta(self):
+        # Encuentra las celdas vacías
+        celdas_vacias = self.encontrar_celdas_vacias()
+
+        # Si no hay celdas vacías, no hacer nada
+        if not celdas_vacias:
+            return
+
+        # Generar una posición inicial en el borde del mapa (por ejemplo, lado izquierdo)
+        x_inicial = 0  # Siempre en la columna 0
+        y_inicial = random.randint(0, len(self.muro.mapa) - 1)
+
+        # Elegir un destino aleatorio entre las celdas vacías
+        x_destino, y_destino = random.choice(celdas_vacias)
+
+        # Colocar la fruta en la posición inicial
+        self.muro.mapa[y_inicial][x_inicial] = 99  # Supongamos que 99 representa una fruta
+
+        # Mover la fruta gradualmente al destino
+        while (x_inicial, y_inicial) != (x_destino, y_destino):
+            self.muro.mapa[y_inicial][x_inicial] = -1  # Limpia la posición actual
+            if x_inicial < x_destino:
+                x_inicial += 1
+            elif x_inicial > x_destino:
+                x_inicial -= 1
+            if y_inicial < y_destino:
+                y_inicial += 1
+            elif y_inicial > y_destino:
+                y_inicial -= 1
+
+            self.muro.mapa[y_inicial][x_inicial] = 99  # Actualiza la posición de la fruta
+            pyxel.flip()  # Actualiza la pantalla para reflejar el cambio
+            time.sleep(0.1)  # Simula el movimiento lento
