f3ecc0b3c745a81ca0ec1b9d8c2e31f552009aa3
fabiannavarroo <fabynf31@gmail.com>
2024-11-29 14:25:46 +0100

    Auto-commit: Fri Nov 29 14:25:46 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -42,24 +42,26 @@ class FantasmaRojo(Fantasma):
                 nueva_x = self.x + self.velocidad
                 if not self.muro.colision(nueva_x, self.y):  # Si no hay colisión
                     self.x = nueva_x
-                    return  # Termina el movimiento
-            elif diferencia_x < 0:  # Pac-Man está a la izquierda
+                    self.direccion_actual = "DERECHA"
+                    
+            else:  # Pac-Man está a la izquierda
                 nueva_x = self.x - self.velocidad
                 if not self.muro.colision(nueva_x, self.y):
                     self.x = nueva_x
-                    return
+                    self.direccion_actual = "IZQUIERDA"
 
         # Si no puede moverse en X, intentar en el eje Y
-        if diferencia_y > 0:  # Pac-Man está abajo
+        else:  # Pac-Man está abajo
             nueva_y = self.y + self.velocidad
             if not self.muro.colision(self.x, nueva_y):
                 self.y = nueva_y
-                return
-        elif diferencia_y < 0:  # Pac-Man está arriba
-            nueva_y = self.y - self.velocidad
-            if not self.muro.colision(self.x, nueva_y):
-                self.y = nueva_y
-                return
+                self.direccion_actual = "ABAJO"
+                
+            else:  # Pac-Man está arriba
+                nueva_y = self.y - self.velocidad
+                if not self.muro.colision(self.x, nueva_y):
+                    self.y = nueva_y
+                    self.direccion_actual = "ARRIBA"
 
         
 
