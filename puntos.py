63948fd6723a01ab1141354f0f8c752366984ddc
fabiannavarroo <fabynf31@gmail.com>
2024-11-29 20:25:23 +0100

    Auto-commit: Fri Nov 29 20:25:23 CET 2024

--- puntos.py
+++ puntos.py
@@ -81,7 +81,7 @@ class Puntos:
                             colkey=0  # Transparencia
                         )
         
-    def draw_puntuacion(self,x,y):
+    def ver_puntuacion(self,x,y):
         # Convierte la puntuación en una str para posteriormente usar una for each y poder pintar cada valor
         puntuacion_str = str(self.puntos)
 
