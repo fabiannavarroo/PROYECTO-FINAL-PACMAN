72ac63a1695f31d9c69d3609a2a89a7ed5722c8d
fabiannavarroo <fabynf31@gmail.com>
2024-11-29 12:31:21 +0100

    Auto-commit: Fri Nov 29 12:31:21 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -72,9 +72,6 @@ class FantasmaRojo(Fantasma):
         diferencia_x = pacman_x - self.x
         diferencia_y = pacman_y - self.y
 
-        print(f"Posición fantasma: x={self.x}, y={self.y}")
-        print(f"Diferencia con Pac-Man: dx={diferencia_x}, dy={diferencia_y}")
-
         # Movimiento en el eje X
         if abs(diferencia_x) > abs(diferencia_y):  # Si la distancia en X es mayor
             if diferencia_x > 0:  # Pac-Man está a la derecha
@@ -82,8 +79,6 @@ class FantasmaRojo(Fantasma):
                 if not self.muro.colision(nueva_x, self.y):  # Verificar colisión
                     self.x = nueva_x
                     self.direccion_actual = "DERECHA"
-                else:
-                    print("Colisión al mover a la derecha")
             else:  # Pac-Man está a la izquierda
                 nueva_x = self.x - self.velocidad
                 if not self.muro.colision(nueva_x, self.y):  # Verificar colisión
@@ -97,15 +92,11 @@ class FantasmaRojo(Fantasma):
                 if not self.muro.colision(self.x, nueva_y):  # Verificar colisión
                     self.y = nueva_y
                     self.direccion_actual = "ABAJO"
-
             else:  # Pac-Man está arriba
                 nueva_y = self.y - self.velocidad
                 if not self.muro.colision(self.x, nueva_y):  # Verificar colisión
                     self.y = nueva_y
                     self.direccion_actual = "ARRIBA"
-                    print("Moviendo hacia arriba")
-                else:
-                    print("Colisión al mover hacia arriba")
 
 class FantasmaRosa(Fantasma):
     def __init__(self, x, y, muro):
