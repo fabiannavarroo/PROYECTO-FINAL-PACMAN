03532afe3cb2d462606e2c995aa114a3c39b9795
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 00:34:56 +0100

    Auto-commit: Sat Nov 30 00:34:56 CET 2024

--- tablero.py
+++ tablero.py
@@ -25,7 +25,7 @@ class Tablero:
     def update(self):
         self.puntos.comer_puntos()
         self.pacman.mover()
-        self.aparecer_fruta()  # Verificar si es momento de generar una fruta
+        self.puntos.aparecer_fruta()  # Verificar si es momento de generar una fruta
         self.fantasmas[0].mover(self.pacman.x, self.pacman.y)
 
     def draw(self):
