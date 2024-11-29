3e2b4618641ec652777a2bf45dcc4ef6d99e13b3
fabiannavarroo <fabynf31@gmail.com>
2024-11-29 18:39:17 +0100

    Auto-commit: Fri Nov 29 18:39:17 CET 2024

--- puntos.py
+++ puntos.py
@@ -3,54 +3,97 @@ from muro import Muro
 import pyxel
 
 class Puntos:
-    def __init__(self, muro, sprite, pacman):
+    def __init__(self,muro,sprite,pacman):
         self.muro = muro
         self.sprite = sprite
         self.pacman = pacman
         self.puntos = 0
 
     def draw(self):
-        # Poner los puntos y otros objetos en el mapa
-        for y in range(len(self.muro.mapa)):
+
+        # Poner Los puntos en el mapa
+        for y in range (len(self.muro.mapa)):
             for x in range(len(self.muro.mapa[y])):
                 if self.muro.mapa[y][x] == 0:
                     sprite = OBJETOS["BASTON"]
                     sprite_x, sprite_y = sprite["Coordenadas"]
-                    sprite_w, sprite_h = 16, 16
+                    sprite_w, sprite_h = 16, 16 # Ancho y largo del sprite
                     pyxel.blt(
-                        x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,
-                        0,
-                        sprite_x, sprite_y,
-                        sprite_w, sprite_h,
-                        colkey=0
+                        x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
+                        0,  # Banco de imágenes
+                        sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
+                        sprite_w, sprite_h,  # Tamaño del sprite
+                        colkey=0  # Transparencia
                     )
-                elif self.muro.mapa[y][x] == 98:
+                
+                # Poner los pastillas de Poder, con su animación
+                if self.muro.mapa[y][x] == 98:
                     if pyxel.frame_count // REFRESH_REGALOS % 2:
                         sprite = OBJETOS["REGALO_BRILLANTE"]
+                        sprite_x, sprite_y = sprite["Coordenadas"]
+                        sprite_w, sprite_h = 16, 16 # Ancho y largo del sprite
+                        
                     else:
                         sprite = OBJETOS["REGALO"]
-                    sprite_x, sprite_y = sprite["Coordenadas"]
-                    sprite_w, sprite_h = 16, 16
-                    pyxel.blt(
-                        x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,
-                        0,
-                        sprite_x, sprite_y,
-                        sprite_w, sprite_h,
-                        colkey=0
-                    )
-
-        # Mostrar la puntuación con texto más grande
-        self.texto_mas_grande(50, 20, f"Puntos: {self.puntos}", 7, 3)
+                        sprite_x, sprite_y = sprite["Coordenadas"]
+                        sprite_w, sprite_h = 16, 16 # Ancho y largo del sprite
+                    
+                    pyxel.blt( # Dibujar los imagenes de los regalos
+                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
+                            0,  # Banco de imágenes
+                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
+                            sprite_w, sprite_h,  # Tamaño del sprite
+                            colkey=0  # Transparencia
+                        )
+                if self.muro.mapa[y][x]==97:
+                    sprite = TEXTO["HIGHSCORE"]
+                    sprite_x,sprite_y = sprite["Coordenadas"]
+                    sprite_w,sprite_h = sprite["Tamaño"]
+                    pyxel.blt( # Dibujar los imagenes de los regalos
+                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
+                            0,  # Banco de imágenes
+                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
+                            sprite_w, sprite_h,  # Tamaño del sprite
+                            colkey=0  # Transparencia
+                        )
+                    
+                if self.muro.mapa[y][x]==96:
+                    sprite = TEXTO["READY!"]
+                    sprite_x,sprite_y = sprite["Coordenadas"]
+                    sprite_w,sprite_h = sprite["Tamaño"]
+                    pyxel.blt( # Dibujar los imagenes de los regalos
+                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
+                            0,  # Banco de imágenes
+                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
+                            sprite_w, sprite_h,  # Tamaño del sprite
+                            colkey=0  # Transparencia
+                        )
+                
+                if self.muro.mapa[y][x]==87:
+                    sprite = TEXTO["GAME OVER"]
+                    sprite_x,sprite_y = sprite["Coordenadas"]
+                    sprite_w,sprite_h = sprite["Tamaño"]
+                    pyxel.blt( # Dibujar los imagenes de los regalos
+                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
+                            0,  # Banco de imágenes
+                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
+                            sprite_w, sprite_h,  # Tamaño del sprite
+                            colkey=0  # Transparencia
+                        )
+        
+                        
+        print(self.texto_mas_grande(50, 20, (self.puntos), 7, 3))  # Escala 3 veces más grande # (x, y, texto, color)
+                    
 
     def comer_puntos(self):
         pacman_x = self.pacman.x // self.muro.celda_tamaño  # Índice X en el mapa
         pacman_y = self.pacman.y // self.muro.celda_tamaño  # Índice Y en el mapa
 
         if self.muro.mapa[pacman_y][pacman_x] in [0, 98]:
-            # Cuando se trata de un punto normal
+            # Cuando se tratra de un punto normal
             if self.muro.mapa[pacman_y][pacman_x] == 0:
                 tipo_consumible = "BASTON"
-            # Cuando es una pastilla de poder
+            # Cuando es es una pastilla de poder
             elif self.muro.mapa[pacman_y][pacman_x] == 98:
                 tipo_consumible = "REGALO"
 
@@ -60,9 +103,10 @@ class Puntos:
             # Eliminar el objeto del mapa
             self.muro.mapa[pacman_y][pacman_x] = -1
 
-    # Hacer texto más grande
+    # Hacer texto más grandes
     def texto_mas_grande(self, x, y, text, color, tamaño):
         for i, char in enumerate(text):
             for dx in range(tamaño):
                 for dy in range(tamaño):
                     pyxel.text(x + i * 4 * tamaño + dx, y + dy, char, color)
+        
