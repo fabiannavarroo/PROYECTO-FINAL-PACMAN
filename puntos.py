a13e22dbd1f689680778e8bc4e20866130502d4f
Bintan20 <ethan.risch@icloud.com>
2024-11-29 10:53:26 +0100

    Auto-commit: 29/11/2024 10:53:26.46

--- puntos.py
+++ puntos.py
@@ -1,4 +1,4 @@
-from constantes import *
+from constantes import OBJETOS, REFRESH_REGALOS, TEXTO, NUMEROS
 from muro import Muro
 import pyxel
 
@@ -152,14 +152,4 @@ class Puntos:
                             sprite_w, sprite_h,  # Tamaño del sprite
                             colkey=0  # Transparencia
                         )
-                if self.muro.mapa[y][x]==86:
-                    sprite = PACMAN_DERECHA
-                    sprite_x,sprite_y = sprite["Coordenadas"]
-                    sprite_w,sprite_h = sprite["Tamaño"]
-                    pyxel.blt( # Dibujar los imagenes de los regalos
-                            x * self.muro.celda_tamaño, y * self.muro.celda_tamaño,  # Coordenadas donde se dibuja el punto
-                            0,  # Banco de imágenes
-                            sprite_x, sprite_y,  # Coordenadas del sprite en recursos.pyxres
-                            sprite_w, sprite_h,  # Tamaño del sprite
-                            colkey=0  # Transparencia
-                        )
+                
