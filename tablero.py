9bfb394c0b03120437e0454cb617bca353c8808c
fabiannavarroo <fabynf31@gmail.com>
2024-11-29 14:39:49 +0100

    Auto-commit: Fri Nov 29 14:39:49 CET 2024

--- tablero.py
+++ tablero.py
@@ -14,10 +14,10 @@ class Tablero:
         self.pacman = Pacman(208, 288, self.muro)  # Coordenadas iniciales del Pacman
         self.puntos= Puntos(self.muro, OBJETOS)
         self.fantasmas = [
-            FantasmaRojo(160, 160, self.muro, self.pacman),  # Coordenadas iniciales en la trampa
-            FantasmaRosa(176, 190, self.muro, self.pacman),
-            FantasmaAzul(192, 190, self.muro, self.pacman),
-            FantasmaNaranja(208, 190, self.muro, self.pacman)
+            FantasmaRojo(160, 160, self.muro),  # Coordenadas iniciales en la trampa
+            FantasmaRosa(176, 190, self.muro),
+            FantasmaAzul(192, 190, self.muro),
+            FantasmaNaranja(208, 190, self.muro)
         ]
 
         pyxel.run(self.update, self.draw)
