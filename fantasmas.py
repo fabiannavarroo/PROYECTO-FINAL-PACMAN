eb9585b7f2e85cf0c0050a615e487c78baa15184
fabiannavarroo <fabynf31@gmail.com>
2024-11-29 12:44:04 +0100

    Auto-commit: Fri Nov 29 12:44:04 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -34,37 +34,37 @@ class FantasmaRojo(Fantasma):
     def mover(self, pacman_x, pacman_y):
         # Calcula la diferencia en X y Y
         diferencia_x = pacman_x - self.x
-    diferencia_y = pacman_y - self.y
+        diferencia_y = pacman_y - self.y
 
-    # Definir prioridad de movimiento
-    if abs(diferencia_x) > abs(diferencia_y):  # Prioridad al eje X
-        if diferencia_x > 0 and not self.muro.colision(self.x + self.velocidad, self.y):
-            self.x += self.velocidad
-            self.direccion_actual = "DERECHA"
-        elif diferencia_x < 0 and not self.muro.colision(self.x - self.velocidad, self.y):
-            self.x -= self.velocidad
-            self.direccion_actual = "IZQUIERDA"
-        else:  # Si no puede moverse en X, intenta en Y
-            if diferencia_y > 0 and not self.muro.colision(self.x, self.y + self.velocidad):
-                self.y += self.velocidad
-                self.direccion_actual = "ABAJO"
-            elif diferencia_y < 0 and not self.muro.colision(self.x, self.y - self.velocidad):
-                self.y -= self.velocidad
-                self.direccion_actual = "ARRIBA"
-    else:  # Prioridad al eje Y
-        if diferencia_y > 0 and not self.muro.colision(self.x, self.y + self.velocidad):
-            self.y += self.velocidad
-            self.direccion_actual = "ABAJO"
-        elif diferencia_y < 0 and not self.muro.colision(self.x, self.y - self.velocidad):
-            self.y -= self.velocidad
-            self.direccion_actual = "ARRIBA"
-        else:  # Si no puede moverse en Y, intenta en X
+        # Definir prioridad de movimiento
+        if abs(diferencia_x) > abs(diferencia_y):  # Prioridad al eje X
             if diferencia_x > 0 and not self.muro.colision(self.x + self.velocidad, self.y):
                 self.x += self.velocidad
                 self.direccion_actual = "DERECHA"
             elif diferencia_x < 0 and not self.muro.colision(self.x - self.velocidad, self.y):
                 self.x -= self.velocidad
                 self.direccion_actual = "IZQUIERDA"
+            else:  # Si no puede moverse en X, intenta en Y
+                if diferencia_y > 0 and not self.muro.colision(self.x, self.y + self.velocidad):
+                    self.y += self.velocidad
+                    self.direccion_actual = "ABAJO"
+                elif diferencia_y < 0 and not self.muro.colision(self.x, self.y - self.velocidad):
+                    self.y -= self.velocidad
+                    self.direccion_actual = "ARRIBA"
+        else:  # Prioridad al eje Y
+            if diferencia_y > 0 and not self.muro.colision(self.x, self.y + self.velocidad):
+                self.y += self.velocidad
+                self.direccion_actual = "ABAJO"
+            elif diferencia_y < 0 and not self.muro.colision(self.x, self.y - self.velocidad):
+                self.y -= self.velocidad
+                self.direccion_actual = "ARRIBA"
+            else:  # Si no puede moverse en Y, intenta en X
+                if diferencia_x > 0 and not self.muro.colision(self.x + self.velocidad, self.y):
+                    self.x += self.velocidad
+                    self.direccion_actual = "DERECHA"
+                elif diferencia_x < 0 and not self.muro.colision(self.x - self.velocidad, self.y):
+                    self.x -= self.velocidad
+                    self.direccion_actual = "IZQUIERDA"
 
     # Si no puede moverse en ninguna dirección, cambia de dirección aleatoria
     if self.direccion_actual == "":
