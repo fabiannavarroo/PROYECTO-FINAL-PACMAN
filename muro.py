b24e52ab960abcdf2b0552e4bc1af95b96c9f940
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 01:54:15 +0100

    Auto-commit: Sat Nov 30 01:54:15 CET 2024

--- muro.py
+++ muro.py
@@ -39,10 +39,10 @@ class Muro:
         
 
 
-    def colision(self, x, y):
+    def colision(self, x, y,tamaño):
         #Comprueba si hay un muro en la posición (x, y) considerando el tamaño del sprite.
         # Tamaño del sprite de Pac-Man y Fantasmas
-        sprite_tamaño = 16
+        sprite_tamaño = tamaño
 
         # Convertir las coordenadas de los bordes del sprite a índices de la matriz
         puntos_a_verificar = [
