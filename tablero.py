19efc55bb15a0e3eb302b7fde006803529db6295
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 19:26:42 +0100

    Auto-commit: Sat Nov 30 19:26:42 CET 2024

--- tablero.py
+++ tablero.py
@@ -10,6 +10,7 @@ class Tablero:
         pyxel.init(430, 415, title="Pacman", display_scale=1, fps=30)
         pyxel.load("assets/recursos.pyxres")
 
+        # Inicializar elementos del juego
         self.muro = Muro()
         self.pacman = Pacman(208, 288, self.muro)
         self.fantasmas = [
@@ -20,13 +21,14 @@ class Tablero:
         ]
         self.puntos = Puntos(self.muro, OBJETOS, self.pacman, self.fantasmas)
 
+        # Iniciar el bucle principal
         pyxel.run(self.update, self.draw)
 
-
     def update(self):
+        # Actualizar estado del juego
         if self.pacman.vidas > 0:
             if self.pacman.en_muerte:
-                self.pacman.animar_muerte()
+                self.pacman.animar_muerte()  # Ejecutar animación de muerte
             else:
                 self.pacman.mover()
                 self.puntos.comer_puntos()
@@ -35,21 +37,25 @@ class Tablero:
                     fantasma.actualizar_estado()
                 self.pacman.colision_fantasmas(self.fantasmas)
         else:
-            self.reiniciar_tablero()
-
+            self.mostrar_game_over()  # Mostrar Game Over si se acaban las vidas
 
     def draw(self):
+        # Dibujar todos los elementos del juego
         pyxel.cls(0)
-        self.puntos.draw()
         self.muro.draw()
+        self.puntos.draw()
         self.pacman.draw()
         for fantasma in self.fantasmas:
             fantasma.draw()
 
-
     def reiniciar_tablero(self):
+        # Reiniciar posiciones de todos los elementos
         self.pacman.reiniciar_posicion()
         self.pacman.en_muerte = False
         self.pacman.animacion_frame = 0
         for fantasma in self.fantasmas:
             fantasma.volver_a_trampa()
+
+    def mostrar_game_over(self):
+        # Mostrar Game Over en el mapa
+        self.pacman.game_over()
