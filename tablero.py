fea4d7ecadbf06a651397a8a97bb95bd5ba7ee80
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 18:50:25 +0100

    Auto-commit: Sat Nov 30 18:50:25 CET 2024

--- tablero.py
+++ tablero.py
@@ -10,44 +10,33 @@ class Tablero:
         pyxel.init(430, 415, title="Pacman", display_scale=1, fps=30)
         pyxel.load("assets/recursos.pyxres")
 
-        # Iniciar el Mapa
         self.muro = Muro()
-
-        # Iniciar el Pacman
         self.pacman = Pacman(208, 288, self.muro)
-
-        # Iniciar los fantasmas
         self.fantasmas = [
             FantasmaRojo(200, 160, self.muro),
             FantasmaRosa(176, 190, self.muro),
             FantasmaAzul(192, 190, self.muro),
             FantasmaNaranja(208, 190, self.muro)
         ]
-
-        # Iniciar los puntos
         self.puntos = Puntos(self.muro, OBJETOS, self.pacman, self.fantasmas)
 
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        # Actualiza el estado del juego
-        if self.pacman.vidas > 0 and not self.pacman.en_muerte:
-            self.pacman.mover()
-            self.puntos.comer_puntos()  # Verificar si Pac-Man comió un punto
-            self.puntos.comer_fruta()  # Verificar si Pac-Man comió una fruta
-
-            # Actualizar estado de fantasmas
-            for fantasma in self.fantasmas:
-                fantasma.actualizar_estado()  # Actualizar su estado (asustado, normal)
-
-            self.pacman.colision_fantasmas(self.fantasmas)  # Verificar colisiones con fantasmas
-        elif self.pacman.en_muerte:
-            self.pacman.animar_muerte()  # Ejecutar la animación de muerte
+        if self.pacman.vidas > 0:
+            if self.pacman.en_muerte:
+                self.pacman.animar_muerte()
+            else:
+                self.pacman.mover()
+                self.puntos.comer_puntos()
+                self.puntos.comer_fruta()
+                for fantasma in self.fantasmas:
+                    fantasma.actualizar_estado()
+                self.pacman.colision_fantasmas(self.fantasmas)
         else:
-            self.reiniciar_tablero()  # Reiniciar tablero si se acaban las vidas
+            self.reiniciar_tablero()
 
     def draw(self):
-        # Dibuja todos los elementos del juego
         pyxel.cls(0)
         self.puntos.draw()
         self.muro.draw()
@@ -56,7 +45,8 @@ class Tablero:
             fantasma.draw()
 
     def reiniciar_tablero(self):
-        # Reiniciar las posiciones de todos los elementos
         self.pacman.reiniciar_posicion()
+        self.pacman.en_muerte = False
+        self.pacman.animacion_frame = 0
         for fantasma in self.fantasmas:
             fantasma.volver_a_trampa()
