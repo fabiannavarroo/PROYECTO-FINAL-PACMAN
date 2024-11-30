3e04a11db194f9dbd67e70fc2d02decb88895dc3
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 16:34:28 +0100

    Auto-commit: Sat Nov 30 16:34:28 CET 2024

--- pacman.py
+++ pacman.py
@@ -112,4 +112,5 @@ class Pacman:
                     self.vidas -= 1  # Si no está asustado, Pacman pierde una vida
                     self.reiniciar_posicion()  # Reiniciar posición
                     if self.vidas <= 0:
+                        self.puntos.dibujar_letras_mapa()
 
