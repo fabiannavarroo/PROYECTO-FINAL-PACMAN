3e568d229618f3752479e6c4f859b9d127e08f76
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 15:27:54 +0100

    Auto-commit: Sat Nov 30 15:27:54 CET 2024

--- tablero.py
+++ tablero.py
@@ -26,7 +26,7 @@ class Tablero:
 
     def update(self):
         self.pacman.mover()
-        self.puntos.actualizar_modo_diablo()
+        self.puntos.comer_regalo()
         self.puntos.comer_puntos()   # Verificar si Pacman comió un punto
         self.puntos.generar_fruta()  # Generar frutas cada 30 segundos
         self.puntos.comer_fruta()    # Verificar si Pacman comió la fruta
