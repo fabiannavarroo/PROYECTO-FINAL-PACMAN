386f5eb2dd4653cb1838129c260d143e189daf71
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 17:27:21 +0100

    Auto-commit: Sat Nov 30 17:27:21 CET 2024

--- pacman.py
+++ pacman.py
@@ -98,7 +98,7 @@ class Pacman:
         # Dibujar el sprite de PacMan
         pyxel.blt(self.x, self.y, 0, sprite_x, sprite_y, 16, 16, colkey=0)
 
-        self.ver_vidas()
+        self.ver_vidas(120, 418)
 
 
     def reiniciar_posicion(self):
