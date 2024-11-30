28c23f0cc734c3461c00f2d93ac228037acf019a
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 17:10:50 +0100

    Auto-commit: Sat Nov 30 17:10:50 CET 2024

--- tablero.py
+++ tablero.py
@@ -36,7 +36,7 @@ class Tablero:
             self.puntos.comer_fruta()    # Verificar si Pacman comio la fruta
             for fantasma in self.fantasmas:
                 fantasma.trampa() # Va actulizando si los fantamas se encuentran en la trampa
-                fantasma.actualizar_estado() # Verifica si los fantasmas estas asustados o nomral
+                fantasma.actualizar_estado() # Verifica si los fantasmas estas asustados o normal
             self.pacman.colision_fantasmas(self.fantasmas)
         else:
             # Mostrar Game Over
