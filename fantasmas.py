3b2ac9c6b37a476828e9c476b5cc92c4a90250d5
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 15:30:54 +0100

    Auto-commit: Sat Nov 30 15:30:54 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -10,44 +10,64 @@ class Fantasma:
         self.y = y
         self.muro = muro
         self.sprites = sprites
-        self.color_actual = sprites["DERECHA"]  # Color inicial basado en la dirección
+        self.color_actual = sprites["DERECHA"]  # Inicializa el color/dirección como "DERECHA"
+        self.direccion_actual = "DERECHA"  # Dirección inicial
         self.asustado = False
         self.tiempo_asustado = 0
 
+    def mover(self):
+        # Movimiento aleatorio
+        opciones_direccion = ["ARRIBA", "ABAJO", "IZQUIERDA", "DERECHA"]
+        self.direccion_actual = random.choice(opciones_direccion)
+
+        nueva_x, nueva_y = self.x, self.y
+
+        if self.direccion_actual == "ARRIBA" and not self.muro.colision(self.x, self.y - 1):
+            nueva_y -= 1
+        elif self.direccion_actual == "ABAJO" and not self.muro.colision(self.x, self.y + 1):
+            nueva_y += 1
+        elif self.direccion_actual == "IZQUIERDA" and not self.muro.colision(self.x - 1, self.y):
+            nueva_x -= 1
+        elif self.direccion_actual == "DERECHA" and not self.muro.colision(self.x + 1, self.y):
+            nueva_x += 1
+
+        self.x, self.y = nueva_x, nueva_y
+
+        # Actualizar el sprite según la dirección
+        if not self.asustado:
+            self.color_actual = self.sprites[self.direccion_actual]
 
     def activar_asustado(self):
         self.asustado = True
         self.tiempo_asustado = time.time()
         self.color_actual = FANTASMAS_ASUSTADOS["AZUL"]["Coordenadas"]  # Cambiar a color asustado
 
-
     def actualizar_estado(self):
         if self.asustado and time.time() - self.tiempo_asustado > 6:
             self.asustado = False
-            self.color_actual = self.sprites["DERECHA"]  # Volver al color inicial
-
+            self.color_actual = self.sprites[self.direccion_actual]  # Restaurar imagen según la dirección
 
     def draw(self):
-        # Dibujar el fantasma con el color actual
+        # Dibujar el fantasma con el sprite correspondiente
         pyxel.blt(self.x, self.y, 0, *self.color_actual, 16, 16, colkey=0)
 
 
 # Subclases de los Fantasmas
 class FantasmaRojo(Fantasma):
     def __init__(self, x, y, muro):
-        super().__init__(x, y, FANTASMA_ROJO, muro)
+        super().__init__(x, y, muro, FANTASMA_ROJO)
 
 
 class FantasmaRosa(Fantasma):
     def __init__(self, x, y, muro):
-        super().__init__(x, y, FANTASMA_ROSA, muro)
+        super().__init__(x, y, muro, FANTASMA_ROSA)
 
 
 class FantasmaAzul(Fantasma):
     def __init__(self, x, y, muro):
-        super().__init__(x, y, FANTASMA_AZUL, muro)
+        super().__init__(x, y, muro, FANTASMA_AZUL)
 
 
 class FantasmaNaranja(Fantasma):
     def __init__(self, x, y, muro):
-        super().__init__(x, y, FANTASMA_NARANJA, muro)
+        super().__init__(x, y, muro, FANTASMA_NARANJA)
