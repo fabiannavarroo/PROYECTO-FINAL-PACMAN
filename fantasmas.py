950c25680735b2de24ed5f1a164fd1acaf82b536
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 15:26:26 +0100

    Auto-commit: Sat Nov 30 15:26:26 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -5,48 +5,31 @@ import time
 
 
 class Fantasma:
-    def __init__(self, x, y, sprites, muro):
-        self.x = x
-        self.y = y
-        self.sprites = sprites  # Imágenes de los fantasmas
-        self.muro = muro  # Referencia a la clase Muro
-        self.velocidad = 1.5  # Velocidad de los fantasmas
-        self.direccion_actual = "DERECHA"  # Comienza moviéndose hacia la derecha
-        self.color_actual = sprites["DERECHA"]  # Color inicial basado en la dirección
-        self.asustado = False
-        self.tiempo_asustado = 0
-
-
-    def draw(self):
-        sprite_x, sprite_y = self.color_actual
-        pyxel.blt(self.x, self.y, 0, sprite_x, sprite_y, 16, 16, colkey=0)
-    
-
-    import time
-
-class Fantasma:
-    def __init__(self, x, y, muro, color_original):
+    def __init__(self, x, y, muro, sprites):
         self.x = x
         self.y = y
         self.muro = muro
-        self.color_original = color_original
+        self.sprites = sprites
+        self.color_actual = sprites["DERECHA"]  # Color inicial basado en la dirección
         self.asustado = False
         self.tiempo_asustado = 0
 
     def activar_asustado(self):
         self.asustado = True
         self.tiempo_asustado = time.time()
+        self.color_actual = FANTASMAS_ASUSTADOS["AZUL"]["Coordenadas"]  # Cambiar a color asustado
 
     def actualizar_estado(self):
         if self.asustado and time.time() - self.tiempo_asustado > 6:
             self.asustado = False
+            self.color_actual = self.sprites["DERECHA"]  # Volver al color inicial
 
     def draw(self):
-        color = FANTASMAS_ASUSTADOS["AZUL"]["Coordenadas"] if self.asustado else self.color_original
-        # Lógica para dibujar el fantasma con su color correspondiente
+        # Dibujar el fantasma con el color actual
+        pyxel.blt(self.x, self.y, 0, *self.color_actual, 16, 16, colkey=0)
 
 
-# Subclases específicas
+# Subclases de los Fantasmas
 class FantasmaRojo(Fantasma):
     def __init__(self, x, y, muro):
         super().__init__(x, y, FANTASMA_ROJO, muro)
