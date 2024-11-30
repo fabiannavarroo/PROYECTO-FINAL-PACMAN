73edd13438a84459ef6bf6efbe762797f0dc8f92
fabiannavarroo <fabynf31@gmail.com>
2024-11-30 21:05:03 +0100

    Auto-commit: Sat Nov 30 21:05:03 CET 2024

--- pacman.py
+++ pacman.py
@@ -109,15 +109,13 @@ class Pacman:
         if self.en_muerte:
             self.animar_muerte(fantasmas)
         else:
-                if self.direccion_actual == PACMAN_ARRIBA:
-                    sprite_x, sprite_y = PACMAN_ARRIBA_CERRADA
-                elif self.direccion_actual == PACMAN_ABAJO:
-                    sprite_x, sprite_y = PACMAN_ABAJO_CERRADA
-                elif self.direccion_actual == PACMAN_IZQUIERDA:
-                    sprite_x, sprite_y = PACMAN_IZQUIERDA_CERRADA
-                elif self.direccion_actual == PACMAN_DERECHA:
-                    sprite_x, sprite_y = PACMAN_DERECHA_CERRADA
-                else:
-                    sprite_x, sprite_y = PACMAN
+            if self.direccion_actual == PACMAN_ARRIBA:
+                sprite_x, sprite_y = PACMAN_ABAJO_CERRADA
+            elif self.direccion_actual == PACMAN_IZQUIERDA:
+                sprite_x, sprite_y = PACMAN_IZQUIERDA_CERRADA
+            elif self.direccion_actual == PACMAN_DERECHA:
+                sprite_x, sprite_y = PACMAN_DERECHA_CERRADA
+            else:
+                sprite_x, sprite_y = PACMAN
 
-                pyxel.blt(self.x, self.y, 0, sprite_x, sprite_y, 16, 16, colkey=0)
+            pyxel.blt(self.x, self.y, 0, sprite_x, sprite_y, 16, 16, colkey=0)
