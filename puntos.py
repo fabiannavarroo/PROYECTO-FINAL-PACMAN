583278651b8b42401913fbd0e0954518f7825dca
Bintan20 <ethan.risch@icloud.com>
2024-12-02 16:12:51 +0100

    Auto-commit: 02/12/2024 16:12:51.03

--- puntos.py
+++ puntos.py
@@ -126,11 +126,14 @@ class Puntos:
 
     def encontrar_celdas_vacias(self):
         # Encuentra celdas vacías en el mapa
+        contador=0
         celdas_vacias = []
         for y in range(len(self.muro.mapa)):
             for x in range(len(self.muro.mapa[y])):
                 if self.muro.mapa[y][x] == -1:
                     celdas_vacias.append((x, y))
+        if len(celdas_vacias)==138:
+            self.victoria=True
         return celdas_vacias
 
 
