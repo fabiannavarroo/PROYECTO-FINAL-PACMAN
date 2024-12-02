b36b7d38b8e8abb0d2a5803db535fef5bba72597
Bintan20 <ethan.risch@icloud.com>
2024-12-02 16:54:39 +0100

    Auto-commit: 02/12/2024 16:54:39.11

--- bloque.py
+++ bloque.py
@@ -1,7 +1,13 @@
 
 
-import constantes
-
+from constantes import *
 class Bloque:
 
-    def __init__(self, x: int, y: int, tipo: int)
+    def __init__(self, x: int, y: int, tipo: int):
+
+        self.x = x
+        self.y = y
+        self.tipo = tipo
+
+        if self.tipo == 1:
+            self.sprite = 
