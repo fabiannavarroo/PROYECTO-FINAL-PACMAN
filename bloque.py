766f1d12590009949c8b6c2200f802001ce81e56
fabiannavarroo <fabynf31@gmail.com>
2024-12-04 20:47:37 +0100

    Auto-commit: Wed Dec  4 20:47:37 CET 2024

--- bloque.py
+++ bloque.py
@@ -1,98 +1,107 @@
 from constantes import *
-import pyxel   
+import pyxel
 
 class Bloque:
-    def __init__(self, x, y, tipo):
-        self.x = x
-        self.y = y
-        self.tipo = tipo
+    def __init__(self):
+        # Lista de bloques inicializada desde MAPA_1
+        self.bloques = []
+        self.celda_tamaño = 16
 
-        # Asignar el sprite basado en el tipo de bloque
+        # Crear bloques a partir de MAPA_1
+        for x, y, tipo in MAPA_1:
+            self.bloques.append({
+                "x": x,
+                "y": y,
+                "sprite": self.obtener_sprite(tipo)
+            })
+
+    def obtener_sprite(self, tipo):
+        # Devuelve el sprite correspondiente al tipo
         if tipo == 1:
-            self.sprite = SPRITE_BLOQUE_1
+            return SPRITE_BLOQUE_1
         elif tipo == 2:
-            self.sprite = SPRITE_BLOQUE_2
+            return SPRITE_BLOQUE_2
         elif tipo == 3:
-            self.sprite = SPRITE_BLOQUE_3
+            return SPRITE_BLOQUE_3
         elif tipo == 4:
-            self.sprite = SPRITE_BLOQUE_4
+            return SPRITE_BLOQUE_4
         elif tipo == 5:
-            self.sprite = SPRITE_BLOQUE_5
+            return SPRITE_BLOQUE_5
         elif tipo == 6:
-            self.sprite = SPRITE_BLOQUE_6
+            return SPRITE_BLOQUE_6
         elif tipo == 7:
-            self.sprite = SPRITE_BLOQUE_7
+            return SPRITE_BLOQUE_7
         elif tipo == 8:
-            self.sprite = SPRITE_BLOQUE_8
+            return SPRITE_BLOQUE_8
         elif tipo == 9:
-            self.sprite = SPRITE_BLOQUE_9
+            return SPRITE_BLOQUE_9
         elif tipo == 10:
-            self.sprite = SPRITE_BLOQUE_10
+            return SPRITE_BLOQUE_10
         elif tipo == 11:
-            self.sprite = SPRITE_BLOQUE_11
+            return SPRITE_BLOQUE_11
         elif tipo == 12:
-            self.sprite = SPRITE_BLOQUE_12
+            return SPRITE_BLOQUE_12
         elif tipo == 13:
-            self.sprite = SPRITE_BLOQUE_13
+            return SPRITE_BLOQUE_13
         elif tipo == 14:
-            self.sprite = SPRITE_BLOQUE_14
+            return SPRITE_BLOQUE_14
         elif tipo == 15:
-            self.sprite = SPRITE_BLOQUE_15
+            return SPRITE_BLOQUE_15
         elif tipo == 16:
-            self.sprite = SPRITE_BLOQUE_16
+            return SPRITE_BLOQUE_16
         elif tipo == 17:
-            self.sprite = SPRITE_BLOQUE_17
+            return SPRITE_BLOQUE_17
         elif tipo == 18:
-            self.sprite = SPRITE_BLOQUE_18
+            return SPRITE_BLOQUE_18
         elif tipo == 19:
-            self.sprite = SPRITE_BLOQUE_19
+            return SPRITE_BLOQUE_19
         elif tipo == 20:
-            self.sprite = SPRITE_BLOQUE_20
+            return SPRITE_BLOQUE_20
         elif tipo == 21:
-            self.sprite = SPRITE_BLOQUE_21
+            return SPRITE_BLOQUE_21
         elif tipo == 22:
-            self.sprite = SPRITE_BLOQUE_22
+            return SPRITE_BLOQUE_22
         elif tipo == 23:
-            self.sprite = SPRITE_BLOQUE_23
+            return SPRITE_BLOQUE_23
         else:
             raise ValueError("Tipo de bloque no válido. Debe estar entre 1 y 23.")
 
     def colision(self, x, y):
-        # Comprueba si hay colisión con las coordenadas (x, y).
-        sprite_w = self.sprite[3]
-        sprite_h = self.sprite[4]
+        # Comprueba si hay colisión con las coordenadas (x, y)
+        sprite_tamaño = 16
 
-        if self.x <= x < self.x + sprite_w and self.y <= y < self.y + sprite_h:
-            return True
-        return False
+        # Puntos de las esquinas del sprite
+        puntos_a_verificar = [
+            (x, y),  # Esquina superior izquierda
+            (x + sprite_tamaño - 1, y),  # Esquina superior derecha
+            (x, y + sprite_tamaño - 1),  # Esquina inferior izquierda
+            (x + sprite_tamaño - 1, y + sprite_tamaño - 1)  # Esquina inferior derecha
+        ]
+
+        # Verificar colisión para cada punto con los bloques
+        for px, py in puntos_a_verificar:
+            for bloque in self.bloques:
+                bloque_x = bloque["x"]
+                bloque_y = bloque["y"]
+                sprite = bloque["sprite"]
+                sprite_w = sprite[3]
+                sprite_h = sprite[4]
+
+                if bloque_x <= px < bloque_x + sprite_w and bloque_y <= py < bloque_y + sprite_h:
+                    return True  # Colisión detectada
+        return False  # No hay colisión
 
     def draw(self):
-        # Dibuja este bloque en la pantalla.
-        sprite_x = self.sprite[0]
-        sprite_y = self.sprite[1]
-        sprite_bank = self.sprite[2]
-        sprite_w = self.sprite[3]
-        sprite_h = self.sprite[4]
+        # Dibuja todos los bloques
+        for bloque in self.bloques:
+            bloque_x = bloque["x"]
+            bloque_y = bloque["y"]
+            sprite = bloque["sprite"]
+            sprite_x = sprite[0]
+            sprite_y = sprite[1]
+            sprite_bank = sprite[2]
+            sprite_w = sprite[3]
+            sprite_h = sprite[4]
 
-        # Dibujar el sprite del bloque
-        pyxel.blt(self.x, self.y, sprite_bank, sprite_x, sprite_y, sprite_w, sprite_h, colkey=0)
-
-    def inicializar_mapa(mapa_data):
-        # Crea instancias de Bloque para un mapa.
-        bloques = []
-        for data in mapa_data:
-            bloque = Bloque(data[0], data[1], data[2])
-            bloques.append(bloque)
-        return bloques
-
-    def dibujar_bloques(bloques):
-        # Dibuja todos los bloques del mapa.
-        for bloque in bloques:
-            bloque.draw()
-
-    def detectar_colision(bloques, x, y):
-        # Detecta si hay colisión en las coordenadas (x, y).
-        for bloque in bloques:
-            if bloque.colision(x, y):
-                return True
-        return False
+            # Dibujar el sprite del bloque
+            pyxel.blt(bloque_x, bloque_y, sprite_bank, sprite_x, sprite_y, sprite_w, sprite_h, colkey=0)
