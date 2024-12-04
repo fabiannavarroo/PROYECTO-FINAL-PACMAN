80f471964628c7d4b149bc3e20ae06318ae095d6
fabiannavarroo <fabynf31@gmail.com>
2024-12-04 21:14:50 +0100

    Auto-commit: Wed Dec  4 21:14:50 CET 2024

--- tablero.py
+++ tablero.py
@@ -8,7 +8,6 @@ import pyxel
 
 class Tablero:
     def __init__(self):
-        self.ya=False
         # Inicializar la ventana del juego con Pyxel
         pyxel.init(430, 415, title="Pacman", display_scale=1, fps=30)  # Crear la pantalla
         pyxel.load("assets/recursos.pyxres")  # Cargar recursos gráficos
