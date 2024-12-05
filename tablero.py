9207ba498bb5c69efda118dc76a1d714dc5a0d22
fabiannavarroo <fabynf31@gmail.com>
2024-12-05 19:27:17 +0100

    Auto-commit: Thu Dec  5 19:27:17 CET 2024

--- tablero.py
+++ tablero.py
@@ -73,7 +73,7 @@ class Tablero:
 
     def dibujar_ready(self):
         # Dibuja el mensaje READY! en el centro de la pantalla
-        if pyxel.frame_count % REFRESH == 0: 
+        if pyxel.frame_count % 10 == 0: 
             sprite = TEXTO["READY!"]
             sprite_x, sprite_y = sprite["Coordenadas"]
             sprite_w, sprite_h = sprite["Tamaño"]
