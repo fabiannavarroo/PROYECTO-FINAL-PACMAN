61546e108bcdeeab7d08356045fe6f97068e5874
fabiannavarroo <fabynf31@gmail.com>
2024-12-05 18:12:27 +0100

    Auto-commit: Thu Dec  5 18:12:27 CET 2024

--- tablero.py
+++ tablero.py
@@ -14,7 +14,7 @@ class Tablero:
         
 
         # Inicializar elementos del juego
-        self.bloque = Bloque(self.puntos) # Mapa del juego
+        self.bloque = Bloque() # Mapa del juego
         self.pacman = Pacman(192, 304, self.bloque)  # Pacman y su posición inicial
         self.fantasmas = [  # Lista de fantasmas con sus posiciones iniciales
             FantasmaRojo(196, 176, ),
