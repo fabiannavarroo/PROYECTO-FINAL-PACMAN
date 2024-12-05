21bf8954082dd1bd7902e210c249bef61b8fdc7a
fabiannavarroo <fabynf31@gmail.com>
2024-12-05 19:15:52 +0100

    Auto-commit: Thu Dec  5 19:15:52 CET 2024

--- tablero.py
+++ tablero.py
@@ -76,8 +76,8 @@ class Tablero:
         sprite = TEXTO["READY!"]
         sprite_x, sprite_y = sprite["Coordenadas"]
         sprite_w, sprite_h = sprite["Tamaño"]
-        pos_x = (pyxel.width - sprite_w) // 2
-        pos_y = (pyxel.height - sprite_h) // 2
+        pos_x = 180
+        pos_y = 240
         pyxel.blt(pos_x, pos_y, 0, sprite_x, sprite_y, sprite_w, sprite_h, colkey=0)
 
     def fin(self):
