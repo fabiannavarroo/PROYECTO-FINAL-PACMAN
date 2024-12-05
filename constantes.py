c14341cb44ed25659db7b335fa771986cd7922c1
fabiannavarroo <fabynf31@gmail.com>
2024-12-05 09:45:06 +0100

    Auto-commit: Thu Dec  5 09:45:06 CET 2024

--- constantes.py
+++ constantes.py
@@ -200,10 +200,10 @@ MAPA_1 = [
     (240,64,2),       # Borde horizontal
     (256,64,2),       # Borde horizontal
     (272,64,2),       # Borde horizontal
-    (0,64,12),      # Esquina superior izquierda 
+    (288,64,12),      # Esquina superior izquierda 
     (320,64,11),      # Esquina superior izquierda
     (336,64,2),       # Borde horizontal
-    (252,64,12),      # Esquina superior derecha
+    (0,64,12),      # Esquina superior derecha
     (384, 64, 5),     # Esquina inferior derecha
 
     # fila 4
