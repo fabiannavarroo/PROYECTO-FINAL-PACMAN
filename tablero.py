062b06c659487b63dc1093d3cb09a3a182cff9a3
fabiannavarroo <fabynf31@gmail.com>
2024-12-05 19:33:14 +0100

    Auto-commit: Thu Dec  5 19:33:14 CET 2024

--- tablero.py
+++ tablero.py
@@ -33,7 +33,7 @@ class Tablero:
     def update(self):
         if self.pacman.vidas > 0:  # Mientras Pacman tenga vidas
             if self.contador_ready > 0:
-                self.contador_ready -= 1  # Reducir el temporizador de READY!
+                self.contador_ready -= 1 
                 if self.contador_ready == 0:
                     self.mostrar_ready = False  # Ocultar READY! después de 3 segundos
 
