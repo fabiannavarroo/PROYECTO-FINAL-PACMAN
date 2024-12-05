c67c7a314a91713efb4257d57b025f0663883f6e
fabiannavarroo <fabynf31@gmail.com>
2024-12-05 17:50:20 +0100

    Auto-commit: Thu Dec  5 17:50:20 CET 2024

--- tablero.py
+++ tablero.py
@@ -47,6 +47,7 @@ class Tablero:
     def draw(self):
         pyxel.cls(0)  # Limpiar pantalla
         if self.pacman.vidas > 0:
+            self.puntos.dibujar_letras_mapa(180,240, "READY!")
             self.bloque.draw() # Dibujar el mapa
             self.puntos.draw()  # Dibujar puntos, frutas y puntuación
             self.pacman.ver_vidas(10, 10)  # Ver las vidas restantes
