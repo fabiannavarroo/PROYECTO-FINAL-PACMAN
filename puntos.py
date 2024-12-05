9cfb8aba011d67972af4c9191ffffbf7989eb5da
fabiannavarroo <fabynf31@gmail.com>
2024-12-05 12:08:34 +0100

    Auto-commit: Thu Dec  5 12:08:34 CET 2024

--- puntos.py
+++ puntos.py
@@ -17,7 +17,8 @@ class Puntos:
         self.posicion_actual = None  # Posición actual de la fruta
         self.animacion_activa = False  # Indica si hay animación activa
         self.animacion_contador = 0  # Contador para animación de aparición
-        self.zonas_prohibidas = [(0,0,384,16), (112,160,272,256), (0,192,80,224),(304,192,384,224)]
+        self.zonas_prohibidas = [(0,0,384,16), (112,160,272,256), (0,192,80,224),(304,192,384,224)] # Zonas prohibidas para generar puntos
+        self.regalos = [(80, 80), (160, 160), (240, 240)] # Coordenadas fijas de los regalos
 
     def draw(self):
         # Poner los puntos en el mapa
