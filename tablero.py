dbe9518c3664ec4d49f8ceb772992166a3e2b8a5
fabiannavarroo <fabynf31@gmail.com>
2024-12-07 02:51:14 +0100

    Auto-commit: Sat Dec  7 02:51:14 CET 2024

--- tablero.py
+++ tablero.py
@@ -60,8 +60,9 @@ class Tablero:
                 self.pacman.animar_muerte(self.fantasmas)
                 if not self.pacman.en_muerte:  # Cuando termina la animación de muerte
                     self.reiniciar_tablero()
-                self.mostrar_fin = True
+                
         else:
+            self.mostrar_fin = True
             self.animar_fin()
         
 
