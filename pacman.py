00f4a34162835e22c33806dbcf49f9315d3babbc
fabiannavarroo <fabynf31@gmail.com>
2024-12-07 03:35:29 +0100

    Auto-commit: Sat Dec  7 03:35:29 CET 2024

--- pacman.py
+++ pacman.py
@@ -126,8 +126,8 @@ class Pacman:
                 if fantasma.asustado:
                     puntos.puntos += 200  # Añade puntos por comer un fantasma
                     self.fantasmas_comido = True
-                    self.mostrar_puntos = True  # Activar la visualización de puntos
-                    self.texto_tiempo_inicio = time.time()  # Registrar el tiempo actual
+                    self.mostrar_puntos = True  # Poder mostrar puntos
+                    self.texto_tiempo_inicio = time.time()  # Guarda el tiempo actual
                     fantasma.volver_a_trampa()  # Enviar fantasma a la trampa
                     return True
                 else:
