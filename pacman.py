a7ac10d5b44fb9662cea8acd2c3700d5ff7571ea
fabiannavarroo <fabynf31@gmail.com>
2024-12-07 03:34:53 +0100

    Auto-commit: Sat Dec  7 03:34:53 CET 2024

--- pacman.py
+++ pacman.py
@@ -48,7 +48,7 @@ class Pacman:
 
             # Mostrar puntos cuando come fantasmas
             if self.mostrar_puntos and time.time() - self.texto_tiempo_inicio < 2:  # Mostrar por 2 segundos
-                pyxel.text(self.x, self.y - 10, "+200 puntos", pyxel.COLOR_YELLOW)
+                pyxel.text(self.x, self.y - 10, "+200 puntos", pyxel.COLOR_CYAN)
             else:
                 self.mostrar_puntos = False
 
