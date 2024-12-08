6ecb420bf6f0233665736dbfd96f455d4d294562
fabiannavarroo <fabynf31@gmail.com>
2024-12-08 02:14:52 +0100

    Auto-commit: Sun Dec  8 02:14:52 CET 2024

--- constantes.py
+++ constantes.py
@@ -187,8 +187,8 @@ SPRITE_BLOQUE_23_1 = (96,88,1,16,16) # Borde inferior trampa fantasma
 
 
 #BLOQUES NIVEL 3
-SPRITE_BLOQUE_1_2 = (80,0,1,16,16) # Esquina izquierda horizontal
-SPRITE_BLOQUE_2_2 = (88,0,1,16,16) # Borde horizontal
+SPRITE_BLOQUE_1_2 = (160,0,1,16,16) # Esquina izquierda horizontal
+SPRITE_BLOQUE_2_2 = (168,0,1,16,16) # Borde horizontal
 SPRITE_BLOQUE_3_2 = (96,0,1,16,16) # Esquina derecha horizontal
 SPRITE_BLOQUE_4_2 = (64,0,1,16,16) # Esquina superior vertical
 SPRITE_BLOQUE_5_2 = (64,8,1,16,16) # Borde vertical
