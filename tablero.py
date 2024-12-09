2a45e170411b9df322df78400e279db6543d58ba
fabiannavarroo <fabynf31@gmail.com>
2024-12-09 16:24:18 +0100

    Auto-commit: Mon Dec  9 16:24:18 CET 2024

--- tablero.py
+++ tablero.py
@@ -223,4 +223,10 @@ class Tablero:
                     return True
 
         return False  # No hay colision
+    
+
+    def dibujar_letras_mapa(self, x, y, sprite):
+        # Dibuja las letras en el mapa
+        sprite = TEXTO[sprite]
+        pyxel.blt(x, y, 0, sprite["Coordenadas"][0], sprite["Coordenadas"][1], sprite["Tamaño"][0], sprite["Tamaño"][1], colkey=0)
 
