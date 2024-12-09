5b09387ffcfe4bb2b596dc4c460cbca9f3eeb16a
fabiannavarroo <fabynf31@gmail.com>
2024-12-09 16:24:01 +0100

    Auto-commit: Mon Dec  9 16:24:01 CET 2024

--- bloque.py
+++ bloque.py
@@ -172,22 +172,6 @@ class Bloque:
                 raise ValueError("Tipo de bloque no válido. Debe estar entre 1 y 23.")
 
 
-    def colision(self, x, y):
-        sprite_tamaño = self.celda_tamaño
-        puntos_a_verificar = [
-            (x, y),  # Esquina superior izquierda
-            (x + sprite_tamaño - 1, y),  # Esquina superior derecha
-            (x, y + sprite_tamaño - 1),  # Esquina inferior izquierda
-            (x + sprite_tamaño - 1, y + sprite_tamaño - 1),  # Esquina inferior derecha
-        ]
-        for px, py in puntos_a_verificar:
-            for bloque_x, bloque_y, _ in self.bloques:
-                if (PUERTA_SALIDA[0] <= px < PUERTA_SALIDA[0] + sprite_tamaño and
-                        PUERTA_SALIDA[1] <= py < PUERTA_SALIDA[1] + sprite_tamaño):
-                    continue  # Ignorar colisión en la puerta de salida
-                if bloque_x <= px < bloque_x + sprite_tamaño and bloque_y <= py < bloque_y + sprite_tamaño:
-                    return True  # Colisión detectada
-        return False  # No hay colisión
 
 
     def draw(self):
