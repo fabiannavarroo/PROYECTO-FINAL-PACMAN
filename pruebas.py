b41f25f39edb340c868da010a93241d5cfef8ab6
fabiannavarroo <fabynf31@gmail.com>
2024-12-11 00:01:55 +0100

    Auto-commit: Wed Dec 11 00:01:55 CET 2024

--- pruebas.py
+++ pruebas.py
@@ -588,7 +588,7 @@ class Tablero:
         # Quita la región de la puerta de salida
         if puerta_x <= x < puerta_x + sprite_tamaño and puerta_y <= y < puerta_y + sprite_tamaño:
             return False  # No hay colisión en la puerta de salida
-        
+
         if self.bloque.colision(x, y) or self.esta_en_zona_prohibida(x, y):
             return True
         return False
