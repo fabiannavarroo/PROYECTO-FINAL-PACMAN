4946edc2d2fe4ed60898738528dec184e33e2cae
fabiannavarroo <fabynf31@gmail.com>
2024-12-11 00:23:26 +0100

    Auto-commit: Wed Dec 11 00:23:26 CET 2024

--- pruebas.py
+++ pruebas.py
@@ -333,7 +333,7 @@ class Tablero:
             self.alejarse_de_pacman(fantasma)  # Movimiento cuando está asustado
         else:
             # Calcular el objetivo de la emboscada
-            objetivo_x, objetivo_y = self.calcular_objetivo_emboscada(fantasma)
+            objetivo_x, objetivo_y = self.calcular_objetivo_emboscada()
 
             # Calcular la ruta hacia el objetivo
             fantasma.siguiente_celda = self.calcular_ruta_fantasma_para_emboscada(fantasma, objetivo_x, objetivo_y)
