e09c1c91f130f9acbf7ceb0b24c8777e2ba52906
fabiannavarroo <fabynf31@gmail.com>
2024-12-10 14:53:55 +0100

    Auto-commit: Tue Dec 10 14:53:55 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -143,7 +143,6 @@ class Fantasma:
 class FantasmaRojo(Fantasma):
     def __init__(self, x, y):
         super().__init__(x, y, FANTASMA_ROJO)
-
        
 class FantasmaRosa(Fantasma):
     def __init__(self, x, y):
