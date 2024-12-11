1c5b2af85047d45bf7319f2a7a272fdaafd041c3
fabiannavarroo <fabynf31@gmail.com>
2024-12-11 20:43:10 +0100

    Auto-commit: Wed Dec 11 20:43:10 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -15,7 +15,7 @@ class Fantasma:
         self.asustado = False  # Indica si está en estado asustado
         self.tiempo_asustado = 0  # Temporizador para estado asustado
         self.tiempo_para_ser_comido = 10  # Duración por defecto del estado asustado
-        self.tiempo_espera = time.time()
+        self.tiempo_espera = time.time() # Tiempo de espera para salir de la trampa
         self.en_salida = False
         self.siguiente_celda = None  # Almacena la próxima celda hacia la que se mueve el fantasma
         self.salida_final = (192, 176)  # Punto fuera de la trampa
