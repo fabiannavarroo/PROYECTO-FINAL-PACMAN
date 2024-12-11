8c69ddf6de939452e9a41ddba7c61a0c43e57fe8
fabiannavarroo <fabynf31@gmail.com>
2024-12-11 21:49:28 +0100

    Auto-commit: Wed Dec 11 21:49:28 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -43,17 +43,13 @@ class Fantasma:
         if abs(dx) > 0:
             if dx > 0:  
                 self.x += self.velocidad 
-                sprite = self.sprites["DERECHA"]
             else:
                 self.x -= self.velocidad
-                sprite = self.sprites["IZQUIERDA"]
         elif abs(dy) > 0:
             if dy > 0:
                 self.y += self.velocidad 
-                sprite = self.sprites["ARRIBA"]
             else:
                 self.y -= self.velocidad
-                sprite = self.sprites["ABAJO"]
         else:
             self.en_salida = False
             self.en_trampa = False  # Marcamos que ha salido de la trampa
