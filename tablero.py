d65ba3f1e09cf9e046566d185535073bf6dcc0b8
fabiannavarroo <fabynf31@gmail.com>
2024-12-11 20:05:29 +0100

    Auto-commit: Wed Dec 11 20:05:29 CET 2024

--- tablero.py
+++ tablero.py
@@ -444,12 +444,6 @@ class Tablero:
                     return True
         return False
 
-    def colision_fantasmas(self, x, y):
-        # Permitir el paso por la puerta de salida
-        if self.fantasmas.puerta_salida[0] <= x < self.fantasmas.puerta_salida[0] + self.bloque.celda_tamaño and \
-        self.fantasmas.puerta_salida[1] <= y < self.fantasmas.puerta_salida[1] + self.bloque.celda_tamaño:
-            return False
-        return self.bloque.colision(x, y)
 
     def detectar_colision_puntos(self, pacman_x, pacman_y, punto_x, punto_y):
         # Detecta si Pac-Man ha comido un punto
