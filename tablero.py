e8c222acf778bb079ec0145fc86dd0801b709eaf
fabiannavarroo <fabynf31@gmail.com>
2024-12-12 00:02:28 +0100

    Auto-commit: Thu Dec 12 00:02:28 CET 2024

--- tablero.py
+++ tablero.py
@@ -354,6 +354,18 @@ class Tablero:
             pass
 
 
+    def mover_fantasma(self, fantasma):
+        # Lógica de movimiento para cada tipo de fantasma
+        if isinstance(fantasma, FantasmaRojo):
+            self.mover_fantasma_rojo(fantasma)
+        elif isinstance(fantasma, FantasmaRosa):
+            self.mover_fantasma_rosa(fantasma)
+        elif isinstance(fantasma, FantasmaAzul):
+            self.mover_fantasma_azul(fantasma)
+        elif isinstance(fantasma, FantasmaNaranja):
+            self.mover_fantasma_naranja(fantasma)
+
+
     #--------------------------------------------------------------------MOVIMIENTO--------------------------------------------------------------------#
 
     def movimineto_pacman(self):
@@ -425,17 +437,8 @@ class Tablero:
 
     
 
-    def mover_fantasma(self, fantasma):
-        # Lógica de movimiento para cada tipo de fantasma
-        if isinstance(fantasma, FantasmaRojo):
-            self.mover_fantasma_rojo(fantasma)
-        elif isinstance(fantasma, FantasmaRosa):
-            self.mover_fantasma_rosa(fantasma)
-        elif isinstance(fantasma, FantasmaAzul):
-            self.mover_fantasma_azul(fantasma)
-        elif isinstance(fantasma, FantasmaNaranja):
-            self.mover_fantasma_naranja(fantasma)
-            
+    
+
 
     def seguir_a_pacman(self, fantasma):
         # El fantasma busca una ruta simple hacia la posición de Pac-Man
