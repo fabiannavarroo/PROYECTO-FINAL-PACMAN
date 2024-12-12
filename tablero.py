33effe94f8f737c52a2f2513c3688292169cad35
fabiannavarroo <fabynf31@gmail.com>
2024-12-12 20:41:10 +0100

    Auto-commit: Thu Dec 12 20:41:10 CET 2024

--- tablero.py
+++ tablero.py
@@ -524,13 +524,31 @@ class Tablero:
 
         if not self.bloque.colision(fantasma.x, fantasma.y) and fantasma.ultima_direccion!= "ARRIBA":
             proxima_celda = ((fantasma.x//16), (fantasma.y//16)-1)
-        elif not self.bloque.colision(fantasma.x, fantasma.y) and fantasma.ultima_direccion!= "ABAJO":
+        if not self.bloque.colision(fantasma.x, fantasma.y) and fantasma.ultima_direccion!= "ABAJO":
             proxima_celda = ((fantasma.x//16), (fantasma.y//16)+1)
-        elif not self.bloque.colision(fantasma.x, fantasma.y) and fantasma.ultima_direccion!= "DERECHA":
+        if not self.bloque.colision(fantasma.x, fantasma.y) and fantasma.ultima_direccion!= "DERECHA":
             proxima_celda = ((fantasma.x//16)+1, (fantasma.y//16))
-        elif not self.bloque.colision(fantasma.x, fantasma.y) and fantasma.ultima_direccion!= "IZQUIERDA":
+        if not self.bloque.colision(fantasma.x, fantasma.y) and fantasma.ultima_direccion!= "IZQUIERDA":
             proxima_celda = ((fantasma.x//16)-1, (fantasma.y//16))
 
+        for celda in ruta:
+            nueva_distancia_x = celda[0] - ubicacion_pacman[0]
+            nueva_distancia_y =celda[1] - ubicacion_pacman[1]
+            if abs(nueva_distancia_x) + abs(nueva_distancia_y) < abs(distancia_x) + abs(distancia_y):
+                distancia_x, distancia_y = nueva_distancia_x, nueva_distancia_y
+                proxima_celda = celda
+
+        if proxima_celda == (fantasma.x//16, (fantasma.y//16)-1):
+            self.mover_fantasma(fantasma, "ARRIBA")
+        elif proxima_celda == (fantasma.x//16, (fantasma.y//16)+1):
+            self.mover_fantasma(fantasma, "ABAJO")
+        elif proxima_celda == ((fantasma.x//16)+1, (fantasma.y//16)):
+            self.mover_fantasma(fantasma, "DERECHA")
+        elif proxima_celda == ((fantasma.x//16)-1, (fantasma.y//16)):
+            self.mover_fantasma(fantasma, "IZQUIERDA")
+
+        ruta.append(proxima_celda)
+        return proxima_celda
 
     def mover_fantasma(self, fantasma, direccion):
         # Mueve al fantasma en la dirección especificada
