3f9195ea7f7983b827271b035f278576ca0baf98
fabiannavarroo <fabynf31@gmail.com>
2024-12-12 16:34:09 +0100

    Auto-commit: Thu Dec 12 16:34:09 CET 2024

--- constantes.py
+++ constantes.py
@@ -80,9 +80,9 @@ TEXTO={
     "READY!": {"Coordenadas": (32, 192), "Tamaño": (64, 16)}, 
     "HIGHSCORE":{"Coordenadas": (0, 168), "Tamaño": (80, 16)}, 
     "GAME OVER": {"Coordenadas": (0, 192), "Tamaño": (32, 24)}, 
-    "VICTORIA_1": {"Coordenadas": (0, 192), "Tamaño": (32, 24)},
-    "VICTORIA_2": {"Coordenadas": (0, 192), "Tamaño": (32, 24)},
-    "VICTORIA_3": {"Coordenadas": (0, 192), "Tamaño": (32, 24)},
+    "VICTORIA_1": {"Coordenadas": (0, 176), "Tamaño": (32, 24)},
+    "VICTORIA_2": {"Coordenadas": (24, 176), "Tamaño": (32, 24)},
+    "VICTORIA_3": {"Coordenadas": (48, 176), "Tamaño": (32, 24)},
 }
 
 
