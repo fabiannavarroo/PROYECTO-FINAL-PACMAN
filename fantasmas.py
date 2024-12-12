eba144695dacdc3caefe265a106cba71ee42616e
fabiannavarroo <fabynf31@gmail.com>
2024-12-12 12:03:44 +0100

    Auto-commit: Thu Dec 12 12:03:44 CET 2024

--- fantasmas.py
+++ fantasmas.py
@@ -20,6 +20,7 @@ class Fantasma:
         self.en_trampa = True  # Indica si está en la trampa
         self.posicion_salida = (192, 176)
         self.siguiente_celda = None
+        self.anterior_celda = None
         
 
 
