ffd8f7b1243da3647f9f19f8116df95fcb0db005
fabiannavarroo <fabynf31@gmail.com>
2024-12-12 20:13:33 +0100

    Auto-commit: Thu Dec 12 20:13:33 CET 2024

--- bloque.py
+++ bloque.py
@@ -173,23 +173,17 @@ class Bloque:
 
 
     def colision(self, x, y):
-        # Verifica si un punto (x, y) colisiona con un bloque del mapa.
         sprite_tamaño = self.celda_tamaño
-        
-        # Verificar los cuatro puntos clave del sprite
         puntos_a_verificar = [
             (x, y),  # Esquina superior izquierda
             (x + sprite_tamaño - 1, y),  # Esquina superior derecha
             (x, y + sprite_tamaño - 1),  # Esquina inferior izquierda
-            (x + sprite_tamaño - 1, y + sprite_tamaño - 1)  # Esquina inferior derecha
+            (x + sprite_tamaño - 1, y + sprite_tamaño - 1),  # Esquina inferior derecha
         ]
-        
-        # Revisar si alguno de los puntos está dentro de un bloque
         for px, py in puntos_a_verificar:
-            celda_x = px // sprite_tamaño
-            celda_y = py // sprite_tamaño
-            if (celda_x, celda_y) in self.bloques:
-                return True  # Hay colisión
+            for bloque_x, bloque_y, _ in self.bloques:
+                if bloque_x <= px < bloque_x + sprite_tamaño and bloque_y <= py < bloque_y + sprite_tamaño:
+                    return True  # Colisión detectada
         return False  # No hay colisión
 
 
