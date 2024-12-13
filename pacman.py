c7756522453d52af7179a0c8b94f6334fe259ca4
fabiannavarroo <fabynf31@gmail.com>
2024-12-14 00:22:09 +0100

    Auto-commit: Sat Dec 14 00:22:09 CET 2024

--- pacman.py
+++ pacman.py
@@ -137,7 +137,28 @@ class Pacman:
 
 #--------------------------------------------------------------------METODOS PROPIOS--------------------------------------------------------------------# 
 
-    
+    def draw(self):
+        if self.vidas <= 0:  # Si no hay vidas, no se dibuja
+            return
+        else:
+            if pyxel.frame_count // REFRESH % 2 == 0:
+                sprite_x, sprite_y = self.direccion_actual
+            else:
+                if self.direccion_actual == PACMAN_ARRIBA:
+                    sprite_x, sprite_y = PACMAN_ARRIBA_CERRADA
+                elif self.direccion_actual == PACMAN_ABAJO:
+                    sprite_x, sprite_y = PACMAN_ABAJO_CERRADA
+                elif self.direccion_actual == PACMAN_IZQUIERDA:
+                    sprite_x, sprite_y = PACMAN_IZQUIERDA_CERRADA
+                elif self.direccion_actual == PACMAN_DERECHA:
+                    sprite_x, sprite_y = PACMAN_DERECHA_CERRADA
+                else:
+                    sprite_x, sprite_y = PACMAN
+            # Dibujar Pac-Man
+            pyxel.blt(self.x, self.y, 0, sprite_x, sprite_y, 16, 16, colkey=0)
+
+            # Dibujar vidas
+            self.ver_vidas(10, 10)
 
     def perder_vida(self):
         self.vidas -= 1
