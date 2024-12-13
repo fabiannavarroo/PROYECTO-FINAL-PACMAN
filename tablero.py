b51422672eb1844e64a8291435152085c687af9f
fabiannavarroo <fabynf31@gmail.com>
2024-12-13 22:15:44 +0100

    Auto-commit: Fri Dec 13 22:15:44 CET 2024

--- tablero.py
+++ tablero.py
@@ -198,7 +198,7 @@ class Tablero:
         # Animación del mensaje GAME OVER cuando se acaban las vidas de Pac-Man
         pyxel.cls(0)
 
-        pyxel.bltm(70,70, 0, 0, 0, 256, 256)  # Tilemap 0, posición (0,0), tamaño 400x400
+        pyxel.bltm(pyxel.width/2-128,pyxel.height/2-128, 0, 0, 0, 256, 256)  # Tilemap 0, posición (0,0), tamaño 400x400
 
 
         if self.bloque.contador_game_over < 70:
