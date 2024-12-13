309858589bbe28794d21bce8adefdd3f88e6fe1b
fabiannavarroo <fabynf31@gmail.com>
2024-12-13 16:43:28 +0100

    Auto-commit: Fri Dec 13 16:43:28 CET 2024

--- tablero.py
+++ tablero.py
@@ -75,8 +75,6 @@ class Tablero:
                     else:
                         # Actualizar movimiento normal si no está en la trampa
                         self.movimiento_fantasmas(fantasma)
-                    # Comprobar colisiones de fantasmas con el mapa
-                    self.bloque.colision(fantasma.x, fantasma.y,)
                     # Actualizar el estado del fantasma 
                     fantasma.actualizar_estado()
 
