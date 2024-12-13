dae7936cf75027a6de907600d831b7d8fd29adbe
fabiannavarroo <fabynf31@gmail.com>
2024-12-13 14:49:24 +0100

    Auto-commit: Fri Dec 13 14:49:24 CET 2024

--- puntos.py
+++ puntos.py
@@ -129,27 +129,26 @@ class Puntos:
     def draw(self):
         # Dibuja los puntos, frutas y regalos en el mapa.
         # Dibujar puntos
-        for x, y, tipo in self.lista_puntos:
+        for x, y, tipo in self.__lista_puntos:
             sprite = OBJETOS[tipo]["Coordenadas"]
             pyxel.blt(x, y, 0, sprite[0], sprite[1], 16, 16, colkey=0)
-
         # Dibujar fruta actual
-        if self.posicion_fruta and self.fruta_actual:
-            if self.animacion_activa and self.animacion_contador < 30:
+        if self.__posicion_fruta and self.__fruta_actual:
+            if self.__animacion_activa and self.__animacion_contador < 30:
                 # Parpadea cada 5 frames
-                if self.animacion_contador// REFRESH % 2 == 0:
-                    sprite = OBJETOS[self.fruta_actual]["Coordenadas"]
-                    pyxel.blt(self.posicion_fruta[0], self.posicion_fruta[1], 0, sprite[0], sprite[1], 16, 16, colkey=0)
-                self.animacion_contador += 1
+                if self.__animacion_contador// REFRESH % 2 == 0:
+                    sprite = OBJETOS[self.__fruta_actual]["Coordenadas"]
+                    pyxel.blt(self.__posicion_fruta[0], self.__posicion_fruta[1], 0, sprite[0], sprite[1], 16, 16, colkey=0)
+                self.__animacion_contador += 1
             else:
                 # Detiene la animación y dibuja la fruta
-                self.animacion_activa = False
-                sprite = OBJETOS[self.fruta_actual]["Coordenadas"]
-                pyxel.blt(self.posicion_fruta[0], self.posicion_fruta[1], 0, sprite[0], sprite[1], 16, 16, colkey=0)
+                self.__animacion_activa = False
+                sprite = OBJETOS[self.__fruta_actual]["Coordenadas"]
+                pyxel.blt(self.__posicion_fruta[0], self.__posicion_fruta[1], 0, sprite[0], sprite[1], 16, 16, colkey=0)
             
 
         # Dibujar regalos
-        for x, y in self.regalos:
+        for x, y in self.__regalos:
             if pyxel.frame_count // REFRESH_REGALOS % 2:
                 coord = OBJETOS["REGALO_BRILLANTE"]["Coordenadas"]
                 pyxel.blt(x, y, 0, coord[0], coord[1], 16, 16, colkey=0)
@@ -161,12 +160,12 @@ class Puntos:
     def ver_puntuacion(self, x, y):
         # Cambia el color solo cuando se supera un nuevo múltiplo de 500
         colores_dispo = [NUMEROS_BLANCOS, NUMEROS_MORADOS, NUMEROS_NARANJAS, NUMEROS_VERDES]
-        if self.puntos // 500 > self.puntos_alcanzados:
-            self.puntos_alcanzados = self.puntos // 500  # Actualiza los puntos alcanzados
-            self.color_actual = random.choice(colores_dispo)  # Elige un nuevo color aleatorio
+        if self.__puntos // 500 > self.__puntos_alcanzados:
+            self.__puntos_alcanzados = self.__puntos // 500  # Actualiza los puntos alcanzados
+            self.__color_actual = random.choice(colores_dispo)  # Elige un nuevo color aleatorio
 
-        color_numeros = self.color_actual
-        puntuacion_str = str(self.puntos)
+        color_numeros = self.__color_actual
+        puntuacion_str = str(self.__puntos)
         pos_x = x
 
         for num in puntuacion_str:
