e0dbb7f4d8a77898540d66b275253705b4631f47
fabiannavarroo <fabynf31@gmail.com>
2024-12-14 01:07:55 +0100

    Auto-commit: Sat Dec 14 01:07:55 CET 2024

--- tablero.py
+++ tablero.py
@@ -55,7 +55,7 @@ class Tablero:
     def draw(self):
         if self.estado_juego == "menu":
             pyxel.cls(0)
-            pyxel.bltm(120, 20, 0, 56, 56, 120, 120)
+            pyxel.bltm(120, 20, 0, 150, 56, 120, 120)
             pyxel.bltm(120, 50, 1, 0, 0, 120, 120)  # Dibuja lo fantsams y al pamcan
             pyxel.text(100, 120, "P: JUGAR", pyxel.COLOR_WHITE)
             pyxel.text(100, 140, "N: VISION REDUCIDA", pyxel.COLOR_WHITE)
