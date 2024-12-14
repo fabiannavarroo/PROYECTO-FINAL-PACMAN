3af959433bf916dc9684d3c45a3d2f4d34021918
fabiannavarroo <fabynf31@gmail.com>
2024-12-14 03:38:55 +0100

    Auto-commit: Sat Dec 14 03:38:55 CET 2024

--- tablero.py
+++ tablero.py
@@ -96,7 +96,23 @@ class Tablero:
     def reinicar_juego(self):
         self.bloque.nivel = 1
         self.pacman.vidas = 3
-        self.bloque.victoria = False
+        self.bloque.victoria = False  
+        self.bloque.victoria_contador_corona = 0
+        self.bloque.victoria_contador_texto = 0
+        self.bloque.mostrar_ready = True
+        self.bloque.contador_ready = 0
+        self.bloque.contador_game_over = 0
+        self.bloque.mostrar_fin = False
+        self.pacman.animacion_frame = 0
+        self.pacman.en_muerte = False
+        self.pacman.reiniciando = False
+        self.pacman.fantasmas_comido = False
+        self.pacman.mostrar_puntos = False
+        self.pacman.texto_tiempo_inicio = 0
+        self.pacman.posicion_fantasma_comido_x,self.pacman.posicion_fantasma_comido_y = None, None
+        self.pacman.animacion_muerte_finalizada = False
+        self.asustado = False  
+        self.tiempo_asustado = 0
         self.reiniciar_tablero()
         self.puntos.reiniciar_puntos(self.bloque)
 
