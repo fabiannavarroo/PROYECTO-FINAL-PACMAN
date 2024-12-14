ed8921a632f93a215c6fcd2cd50c9db52de8e4c7
fabiannavarroo <fabynf31@gmail.com>
2024-12-14 02:02:33 +0100

    Auto-commit: Sat Dec 14 02:02:33 CET 2024

--- tablero.py
+++ tablero.py
@@ -58,8 +58,8 @@ class Tablero:
             pyxel.bltm(10, -60, 0, 0, 360, 390, 150, colkey=0)
             pyxel.bltm(120, 50, 1, 0, 0, 120, 120, colkey=0)  # Dibuja lo fantsams y al pamcan
             pyxel.blt(120, 250, 0, 88, 160, 56, 8, colkey=0)  # P: Jugar
-            pyxel.blt(120, 280, 0, 88, 168, 56, 8, colkey=0)  # N: Vision reducida
-            pyxel.blt(120, 300, 0, 88, 176, 56, 8, colkey=0)  # N: Vision reducida
+            pyxel.blt(120, 280, 0, 88, 168, 100, 8, colkey=0)  # N: Vision reducida
+            pyxel.blt(120, 300, 0, 88, 176, 100, 8, colkey=0)  # N: Vision reducida
 
         elif self.estado_juego == "jugando":
             self.modo_juego_normal_dibujo()
