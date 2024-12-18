cd6e8a6a18c376663230adc3a6afb3479bf7c455
fabiannavarroo <fabynf31@gmail.com>
2024-12-18 23:08:06 +0100

    Auto-commit: Wed Dec 18 23:08:06 CET 2024

--- tablero.py
+++ tablero.py
@@ -42,7 +42,7 @@ class Tablero:
         if pyxel.btnp(pyxel.KEY_A):
             self.bloque.nivel += 1
             self.reiniciar_tablero()
-            self.puntos.reiniciar_puntos()
+            self.puntos.reiniciar_puntos(self.bloque)
             
 
         if self.estado_juego == "menu":
